673c792d46fc5f4574d8e07fd64bb3b188358938
jonas schell <jonasschell@ocupe.org>
2022-08-30 16:17:04 +0200

    add styles

--- examples/nextjs/pages/_app.tsx
+++ examples/nextjs/pages/_app.tsx
@@ -1,4 +1,5 @@
 import '../styles/globals.css';
+import '@livekit/components-styles/dist/styles.css';
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {

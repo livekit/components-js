a165558709eb997e19dac017c0d35fb74b69b6e4
jonas schell <jonasschell@ocupe.org>
2022-09-02 12:09:17 +0200

    cleanup

--- packages/react/src/components/ConnectionQuality.tsx
+++ packages/react/src/components/ConnectionQuality.tsx
@@ -1,12 +1,14 @@
-import React, { HTMLAttributes, useCallback, useEffect, useMemo, useState } from 'react';
+import React, { HTMLAttributes, useEffect, useMemo, useState } from 'react';
 import { setupConnectionQualityIndicator } from '@livekit/components-core';
 import { useParticipantContext } from './ParticipantRenderer';
-import { Participant, ConnectionQuality } from 'livekit-client';
+import { ConnectionQuality } from 'livekit-client';
 import { mergeProps } from 'react-aria';
 
-const useConnectionQualityIndicator = (props: HTMLAttributes<HTMLDivElement>) => {
-  const participant: Participant = useParticipantContext();
-  const [quality, setQuality] = useState<ConnectionQuality>(ConnectionQuality.Unknown);
+interface ConnectionIndicatorProps extends HTMLAttributes<HTMLDivElement> {}
+
+const useConnectionIndicator = (props: ConnectionIndicatorProps) => {
+  const participant = useParticipantContext();
+  const [quality, setQuality] = useState(ConnectionQuality.Unknown);
   const { className, onConnectionQualityChange } = useMemo(
     () => setupConnectionQualityIndicator(),
     [],
@@ -14,19 +16,18 @@ const useConnectionQualityIndicator = (props: HTMLAttributes<HTMLDivElement>) =>
 
   const divProps = useMemo(() => mergeProps(props, { className }), [props, className]);
 
-  const handleUpdate = useCallback((quality: ConnectionQuality) => {
-    setQuality(quality);
-  }, []);
-
   useEffect(() => {
-    return onConnectionQualityChange(participant, handleUpdate);
+    return onConnectionQualityChange(participant, (quality) => setQuality(quality));
   }, [participant]);
 
   return { divProps, quality };
 };
 
-export const ConnectionQualityIndicator = (props: HTMLAttributes<HTMLDivElement>) => {
-  const { divProps, quality } = useConnectionQualityIndicator(props);
+/**
+ * Display the connection quality of a given participant.
+ */
+export const ConnectionIndicator = (props: HTMLAttributes<HTMLDivElement>) => {
+  const { divProps, quality } = useConnectionIndicator(props);
   return (
     <div {...divProps} className={divProps.className + ` lk-${quality}`}>
       {props.children}

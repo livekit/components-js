a168ee9b894362c933d03e46b29fc6bdd5514439
Lukas <mail@lukasseiler.de>
2022-09-02 17:11:07 +0200

    extract logic into hook

--- packages/react/src/components/ConnectionQuality.tsx
+++ packages/react/src/components/ConnectionQuality.tsx
@@ -1,39 +1,55 @@
 import React, { HTMLAttributes, useEffect, useMemo, useState } from 'react';
 import { ConnectionQualityInterface } from '@livekit/components-core';
 import { useParticipantContext } from './Participant';
-import { ConnectionQuality } from 'livekit-client';
+import { ConnectionQuality, Participant } from 'livekit-client';
 import { mergeProps } from 'react-aria';
 
 interface ConnectionIndicatorProps extends HTMLAttributes<HTMLDivElement> {}
+
+function isProp(prop: HTMLAttributes<any> | undefined): prop is HTMLAttributes<any> {
+  return prop !== undefined;
+}
+
+const betterMergeProps = (...props: Array<HTMLAttributes<any> | undefined>) => {
+  return mergeProps(...props.filter(isProp));
+};
+
+export const useConnectionQuality = (
+  props?: ConnectionIndicatorProps,
+  participant?: Participant,
+) => {
+  const p = participant ?? useParticipantContext();
+
+  const [quality, setQuality] = useState(ConnectionQuality.Unknown);
+
+  const { staticProps, createConnectionQualityObserver } = useMemo(() => {
+    const { className } = ConnectionQualityInterface.setup();
+    const { createConnectionQualityObserver } = ConnectionQualityInterface.observers();
+    return {
+      staticProps: betterMergeProps(props, { className }),
+      createConnectionQualityObserver,
+    };
+  }, []);
+
+  useEffect(() => {
+    const subscription = createConnectionQualityObserver(p).subscribe(setQuality);
+    return () => {
+      subscription.unsubscribe();
+    };
+  }, [p]);
+
+  const elementProps = useMemo(
+    () => betterMergeProps(props, staticProps, { className: `lk-${quality}` }),
+    [quality, props, staticProps],
+  );
+
+  return { elementProps, quality };
+};
 
 /**
  * Display the connection quality of a given participant.
  */
 export const ConnectionIndicator = (props: ConnectionIndicatorProps) => {
-  const { className } = ConnectionQualityInterface.setup();
-  const mergedProps = useMemo(() => mergeProps(props, { className }), [props, className]);
-  const participant = useParticipantContext();
-  const [quality, setQuality] = useState(ConnectionQuality.Unknown);
-
-  // Get al create-observer-functions that are needed for a component.
-  const { createConnectionQualityObserver } = useMemo(
-    () => ConnectionQualityInterface.observers(),
-    [participant],
-  );
-
-  // Create and subscribe to observers.
-  useEffect(() => {
-    const subscription = createConnectionQualityObserver(participant).subscribe((quality) => {
-      setQuality(quality);
-    });
-    return () => {
-      subscription.unsubscribe();
-    };
-  }, [participant]);
-
-  return (
-    <div {...mergedProps} className={className + ` lk-${quality}`}>
-      {props.children}
-    </div>
-  );
+  const { elementProps } = useConnectionQuality(props);
+  return <div {...elementProps}>{props.children}</div>;
 };

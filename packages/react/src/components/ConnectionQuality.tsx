bbbb33e843f63e0b8069667f4aebe0b2890060f8
Lukas <mail@lukasseiler.de>
2022-09-02 12:45:57 +0200

    fix import

--- packages/react/src/components/ConnectionQuality.tsx
+++ packages/react/src/components/ConnectionQuality.tsx
@@ -1,6 +1,6 @@
 import React, { HTMLAttributes, useEffect, useMemo, useState } from 'react';
 import { setupConnectionQualityIndicator } from '@livekit/components-core';
-import { useParticipantContext } from './ParticipantRenderer';
+import { useParticipantContext } from './Participant';
 import { ConnectionQuality } from 'livekit-client';
 import { mergeProps } from 'react-aria';
 
@@ -26,7 +26,7 @@ const useConnectionIndicator = (props: ConnectionIndicatorProps) => {
 /**
  * Display the connection quality of a given participant.
  */
-export const ConnectionIndicator = (props: HTMLAttributes<HTMLDivElement>) => {
+export const ConnectionQualityIndicator = (props: HTMLAttributes<HTMLDivElement>) => {
   const { divProps, quality } = useConnectionIndicator(props);
   return (
     <div {...divProps} className={divProps.className + ` lk-${quality}`}>

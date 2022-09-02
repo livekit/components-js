40133800bf7f4d816eab80a22890837f8e1e94cd
Lukas <mail@lukasseiler.de>
2022-09-02 11:36:49 +0200

    use useMemo approach to avoid re-computing setup

--- packages/react/src/components/ConnectionQuality.tsx
+++ packages/react/src/components/ConnectionQuality.tsx
@@ -1,4 +1,4 @@
-import React, { HTMLAttributes, useCallback, useEffect, useState } from 'react';
+import React, { HTMLAttributes, useCallback, useEffect, useMemo, useState } from 'react';
 import { setupConnectionQualityIndicator } from '@livekit/components-core';
 import { useParticipantContext } from './ParticipantRenderer';
 import { Participant, ConnectionQuality } from 'livekit-client';
@@ -7,8 +7,12 @@ import { mergeProps } from 'react-aria';
 const useConnectionQualityIndicator = (props: HTMLAttributes<HTMLDivElement>) => {
   const participant: Participant = useParticipantContext();
   const [quality, setQuality] = useState<ConnectionQuality>(ConnectionQuality.Unknown);
-  const { className, onConnectionQualityChange } = setupConnectionQualityIndicator();
-  const mergedProps = mergeProps(props, { className });
+  const { className, onConnectionQualityChange } = useMemo(
+    () => setupConnectionQualityIndicator(),
+    [],
+  );
+
+  const divProps = useMemo(() => mergeProps(props, { className }), [props, className]);
 
   const handleUpdate = useCallback((quality: ConnectionQuality) => {
     setQuality(quality);
@@ -18,7 +22,7 @@ const useConnectionQualityIndicator = (props: HTMLAttributes<HTMLDivElement>) =>
     return onConnectionQualityChange(participant, handleUpdate);
   }, [participant]);
 
-  return { divProps: mergedProps, quality };
+  return { divProps, quality };
 };
 
 export const ConnectionQualityIndicator = (props: HTMLAttributes<HTMLDivElement>) => {

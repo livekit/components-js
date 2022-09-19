685c33345fa2d3f8c5bdf559f13571e33b915102
Lukas <mail@lukasseiler.de>
2022-09-19 16:14:57 +0200

    add initial svelte/sveltekit setup

---
+++ packages/svelte/src/ConnectionState.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { connectionStateObserver } from "@livekit/components-core";
+    import type {Room} from "livekit-client";
+	import { getRoomContext } from "./contexts";
+
+    export let room: Room | undefined = undefined;
+
+    const connectionState = connectionStateObserver(room ?? getRoomContext())
+</script>
+
+<span>{$connectionState}</span>

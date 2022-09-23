c3aaf239fed7ac89ef459e579bfa5fe9e520b57c
Lukas <mail@lukasseiler.de>
2022-09-23 17:48:13 +0200

    fixme note

--- packages/shared/src/component-interfaces/mediaMutedIndicator.ts
+++ packages/shared/src/component-interfaces/mediaMutedIndicator.ts
@@ -1,8 +1,11 @@
+import { ClassNames } from '@livekit/components-styles/dist/types/styles.scss';
 import { Participant, Track } from 'livekit-client';
 import { mutedObserver } from '../observables/participant';
 
 export function setupMediaMutedIndicator(participant: Participant, source: Track.Source) {
-  const className = `lk-media-muted-indicator-${source}`;
+  // FIXME
+  // @ts-ignore
+  const className: ClassNames = `lk-media-muted-indicator-${source}`;
   const mediaMutedObserver = mutedObserver(participant, source);
 
   return { className, mediaMutedObserver };

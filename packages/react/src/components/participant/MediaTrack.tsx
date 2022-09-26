91d352ac14e41df4f0126a4b48b8ab00ab2f7c17
lukasIO <mail@lukasseiler.de>
2022-09-26 15:24:06 +0200

    initial work for layout components (#33)

--- packages/react/src/components/participant/MediaTrack.tsx
+++ packages/react/src/components/participant/MediaTrack.tsx
@@ -2,28 +2,30 @@ import { Participant, Track } from 'livekit-client';
 import React, { useRef } from 'react';
 import { useParticipantContext } from '../../contexts';
 import { LKComponentAttributes } from '../../utils';
-import { useMediaTrack } from './Participant';
+import { ParticipantClickEvent, useMediaTrack } from './Participant';
 
 export interface MediaTrackProps<T extends HTMLMediaElement = HTMLMediaElement>
   extends Omit<LKComponentAttributes<T>, 'children'> {
   participant?: Participant;
   source: Track.Source;
+  onTrackClick?: (evt: ParticipantClickEvent) => void;
 }
 
-export const MediaTrack = (props: MediaTrackProps) => {
+export const MediaTrack = ({ onTrackClick, onClick, ...props }: MediaTrackProps) => {
   const participant = props.participant ?? useParticipantContext();
 
   const mediaEl = useRef<HTMLVideoElement>(null);
   const { elementProps, publication } = useMediaTrack(participant, props.source, mediaEl);
 
+  const clickHandler = (evt: React.MouseEvent<HTMLMediaElement, MouseEvent>) => {
+    onClick?.(evt);
+    onTrackClick?.({ participant, publication });
+  };
+
   return (
     <>
       {props.source === Track.Source.Camera || props.source === Track.Source.ScreenShare ? (
-        <video
-          ref={mediaEl}
-          {...elementProps}
-          onClick={(evt) => props.onClick?.({ ...evt, participant, publication })}
-        ></video>
+        <video ref={mediaEl} {...elementProps} onClick={clickHandler}></video>
       ) : (
         <audio ref={mediaEl} {...elementProps}></audio>
       )}

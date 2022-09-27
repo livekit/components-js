1c5fb146348a729c368d3387abf609e4b9bb099d
jonas schell <jonasschell@ocupe.org>
2022-09-27 14:52:17 +0200

    fix regex

--- packages/styles/postcss-plugins/data-attribute-prefixer.js
+++ packages/styles/postcss-plugins/data-attribute-prefixer.js
@@ -3,15 +3,14 @@
  * @type {import('postcss').PluginCreator}
  */
 module.exports = (opts = { prefix: '' }) => {
-  const findRegex = new RegExp(`/[data-(?=[^]${opts.prefix}])/g`);
   return {
     postcssPlugin: 'data-attribute-prefixer',
-    Root(root, postcss) {
-      console.log(' Running Plugin: data-attribute-prefixer');
-    },
-
     Rule(rule) {
-      rule.selector = rule.selector.replace(findRegex, `[data-${opts.prefix}`);
+      // Regex tests here: regexr.com/6ut26
+      rule.selector = rule.selector.replace(
+        `[data-(?=[^]])(?!${opts.prefix})`,
+        `[data-${opts.prefix}`,
+      );
     },
   };
 };

8cd28f8f759ba99374c7e2aaaf0168626aef5232
jonas schell <jonasschell@ocupe.org>
2022-09-27 15:28:38 +0200

    fix regex again :D

--- packages/styles/postcss-plugins/data-attribute-prefixer.js
+++ packages/styles/postcss-plugins/data-attribute-prefixer.js
@@ -2,15 +2,14 @@
  *
  * @type {import('postcss').PluginCreator}
  */
-module.exports = (opts = { prefix: '' }) => {
+module.exports = (opts = { prefix: 'NO_PREFIX_DEFINED' }) => {
   return {
     postcssPlugin: 'data-attribute-prefixer',
     Rule(rule) {
       // Regex tests here: regexr.com/6ut26
-      rule.selector = rule.selector.replace(
-        `[data-(?=[^]])(?!${opts.prefix})`,
-        `[data-${opts.prefix}`,
-      );
+      const findThis = new RegExp('\\[data-(?=[^\\]])(?!' + opts.prefix + ')');
+      const replaceWith = '[data-' + opts.prefix;
+      rule.selector = rule.selector.replace(findThis, replaceWith);
     },
   };
 };

1a29c1e6f5964ec17b4f03da74585c6633eb2ba3
jonas schell <jonasschell@ocupe.org>
2022-09-30 15:11:49 +0200

    add avatar if video is muted

--- examples/nextjs/pages/huddle.tsx
+++ examples/nextjs/pages/huddle.tsx
@@ -85,6 +85,16 @@ const Huddle: NextPage = () => {
                   />
                   <ParticipantName className={styles.name}></ParticipantName>
                 </div>
+
+                <svg width="100%" height="100%" viewBox="0 -10 10 10">
+                  <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    fill="white"
+                    stroke="none"
+                    d="M 1 0 Q 1 -3 5 -3 Q 9 -3 9 0 L 1 0 Z M 5 -4 A 1 1 0 0 0 5 -8 A 1 1 0 0 0 5 -4"
+                  ></path>
+                </svg>
               </ParticipantView>
             </Participants>
           </div>

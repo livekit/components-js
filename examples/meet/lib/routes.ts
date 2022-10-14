13dc9d1ec078923ce27556b237966437ac381d2b
Lukas <mail@lukasseiler.de>
2022-10-14 13:32:42 +0200

    remove unneeded files

--- examples/meet/lib/routes.ts
+++
@@ -1,8 +0,0 @@
-export const EXAMPLE_ROUTES = {
-  customize: '/customize',
-  huddle: '/huddle',
-  lowlevel: '/lowlevel',
-  minimal: '/minimal',
-  simple: '/simple',
-  oldindex: '/oldindex',
-};

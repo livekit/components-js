bbf596388cea24aed967b843f1baa1d313056d8f
Lukas <mail@lukasseiler.de>
2022-10-17 16:07:06 +0200

    move things into theme, add prejoin

--- examples/nextjs/pages/_app.tsx
+++ examples/nextjs/pages/_app.tsx
@@ -2,7 +2,7 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import '@livekit/components-styles';
 import '@livekit/components-styles/layout';
-import '@livekit/components-styles/themes/rounded';
+import '@livekit/components-styles/themes/huddle';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;

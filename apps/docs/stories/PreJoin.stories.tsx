e7fed31540dc3fad7ccc002d05e7e0b89df99675
Jonas Schell <jonas@livekit.io>
2022-10-24 16:20:13 +0200

    set initial value

--- apps/docs/stories/PreJoin.stories.tsx
+++ apps/docs/stories/PreJoin.stories.tsx
@@ -11,25 +11,8 @@ export default {
 };
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template = (args) => <PreJoin {...args} />;
+const Template = (args: typeof PreJoin) => <PreJoin {...args} />;
 
 export const Default = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
-Default.args = {};
-
-// export const Secondary = Template.bind({});
-// Secondary.args = {
-//   label: 'Button',
-// };
-
-// export const Large = Template.bind({});
-// Large.args = {
-//   size: 'large',
-//   label: 'Button',
-// };
-
-// export const Small = Template.bind({});
-// Small.args = {
-//   size: 'small',
-//   label: 'Button',
-// };
+Default.args = { defaults: { username: 'Initial Name' } };

b51beaa7f41ed33af7f7d45de1ed138ba3e0ed96
Jonas Schell <jonas@livekit.io>
2022-10-25 16:50:45 +0200

    cleanup and use storybook 7 features

--- apps/docs/.storybook/main.ts
+++ apps/docs/.storybook/main.ts
@@ -1,5 +1,5 @@
 module.exports = {
-  stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['../stories'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',

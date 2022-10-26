ae65dc66f9df7eda02aed7c5d794cdb1e5d0fe8e
Jonas Schell <jonas@livekit.io>
2022-10-26 10:33:07 +0200

    add DefaultControls story

---
+++ apps/docs/stories/prefabs/DefaultControls.stories.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StoryObj } from '@storybook/react';
+
+import { DefaultControls, DefaultControlsProps } from '@livekit/components-react';
+import { LkRoomContext } from '../../.storybook/LkRoomContext';
+
+export default {
+  component: DefaultControls,
+  decorators: [LkRoomContext],
+  render: (args: DefaultControlsProps) => <DefaultControls {...args} />,
+  argTypes: {},
+  parameters: {
+    actions: {
+      handles: [],
+    },
+  },
+};
+
+export const Default: StoryObj<DefaultControlsProps> = {
+  args: {},
+  parameters: { roomContext: { audio: true, video: true, connect: true } },
+};

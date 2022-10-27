3a4e16f4d8de569b6ff92f596262b899a8fde7db
Jonas Schell <jonas@livekit.io>
2022-10-27 17:00:14 +0200

    Add better docs

--- scripts/update-nav-in-readmes.js
+++ scripts/update-nav-in-readmes.js
@@ -15,22 +15,28 @@ const NAVIGATION_BLOCK = `<!--NAV_START-->
     * [Vue](/packages/vue/README.md)
 <!--NAV_END-->`;
 
+/**
+ * Based on the path determine if an link to that path should be annotated with a emoji.
+ */
 function getNavigationBlock(path) {
   const findThis = new RegExp('\\[(.*)\\](?=\\(/?' + path + '\\))', 'g');
   const updatedNavBlock = NAVIGATION_BLOCK.replaceAll(findThis, '[$1 👈]');
-  if (updatedNavBlock !== NAVIGATION_BLOCK) {
-    console.log(`Updated nav block for: ${path}`);
-  } else {
-    console.log(`Nothing to update whit RegEx: ${findThis}`);
-  }
+  // if (updatedNavBlock !== NAVIGATION_BLOCK) {
+  //   console.log(`Updated nav block for: ${path}`);
+  // } else {
+  //   console.log(`\t No match was found for regex: ${findThis}`);
+  // }
   return updatedNavBlock;
 }
 
+/**
+ * Replace everything between <!--NAV_START--> and <!--NAV_END--> and write it to file.
+ */
 function replaceNavigationBlock(path) {
   fs.readFile(path, 'utf-8', (err, data) => {
     if (err) throw err;
     getNavigationBlock(path);
-    const findThis = new RegExp('<!--NAV_START-->((.|\\n)+?)<!--NAV_END-->', 'g');
+    const findThis = new RegExp('<!--NAV_START-->((.|\\n)*?)<!--NAV_END-->', 'g');
     const newContent = data.replaceAll(findThis, getNavigationBlock(path));
     if (data !== newContent) {
       fs.writeFile(path, newContent, 'utf-8', (err) => {
@@ -48,7 +54,7 @@ function main() {
     '\nUpdate navigation blocks in all README.md files...\n---------------------------------------------\n',
   );
   glob('**/README.md', { ignore: ['**/node_modules/**/*', '**/dist/**/*'] }, (err, files) => {
-    console.log('📝 Files matching Regex:\n', files);
+    console.log(`📝 Files matching Regex (count ${files.length}):\n`, files);
     if (err) throw err;
     files.map((path) => {
       replaceNavigationBlock(path);

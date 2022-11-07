77b54be36a410eabe78a35eb985c1193ea56361a
Lukas <mail@lukasseiler.de>
2022-11-07 13:40:50 +0100

    remove svelte

--- packages/svelte/src/contexts.ts
+++
@@ -1,7 +0,0 @@
-import type { Room } from 'livekit-client';
-import { getContext, setContext } from 'svelte';
-
-export const roomContextKey = `lk-room-context`;
-
-export const setRoomContext = (room: Room) => setContext(roomContextKey, room);
-export const getRoomContext = () => getContext(roomContextKey) as Room;

2edcf0f09ea0e40e4455d387c08f10fda6679c71
Lukas <mail@lukasseiler.de>
2022-11-07 13:39:33 +0100

    remove vue

--- packages/vue/src/components/Participants.vue
+++
@@ -1,7 +0,0 @@
-<script lang="ts" setup>
-    import { watch } from 'vue';
-import { useParticipants } from './useParticipants';
-    const participants = useParticipants();
-
-    watch(participants, (p) => console.log("participants updated", p.length))
-</script>

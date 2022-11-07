399833f8abe28f13d367ecdce6c234b0ea74304b
lukasIO <mail@lukasseiler.de>
2022-11-07 13:41:09 +0100

    remove svelte (#78)

--- examples/sveltekit/src/routes/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-  import { ConnectionState, LiveKitRoom } from '@livekit/components-svelte';
-  const serverUrl = import.meta.env.VITE_PUBLIC_LK_SERVER_URL;
-  const devToken = import.meta.env.VITE_PUBLIC_LK_DEV_TOKEN;
-</script>
-
-<LiveKitRoom {serverUrl} token={devToken}>
-  <ConnectionState />
-</LiveKitRoom>
--- packages/svelte/src/index.ts
+++
@@ -1,5 +0,0 @@
-// Reexport your entry components here
-import ConnectionState from './ConnectionState.svelte';
-import LiveKitRoom from './LiveKitRoom.svelte';
-
-export { LiveKitRoom, ConnectionState };

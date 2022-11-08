4e10bb33c6363b63514cddedf8a6930b9aec48dc
Lukas <mail@lukasseiler.de>
2022-11-08 16:01:56 +0100

    back to tsup

--- packages/react/tsup.config.ts
+++ packages/react/tsup.config.ts
@@ -4,6 +4,5 @@ import defaults from '../../tsup.config';
 
 export default defineConfig({
   ...defaults,
-  entry: ['src/index.ts', 'src/token.ts'],
   external: ['livekit-client', 'react', 'react-dom'],
 });

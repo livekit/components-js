94e42ac9c6cdf8cdf5ded1a20b99e7b66ac079ce
Jonas Schell <jonas@livekit.io>
2022-11-24 18:57:53 +0100

    Add GridView docstring

--- packages/react/src/components/layout/GridView.tsx
+++ packages/react/src/components/layout/GridView.tsx
@@ -5,11 +5,30 @@ import { ParticipantView } from '../participant/Participant';
 import { Participants } from '../Participants';
 
 export interface GridViewProps extends React.HTMLAttributes<HTMLDivElement> {
-  participants?: Array<Participant>;
+  /**
+   * If set to `true` screen shares are displayed with the participants.
+   */
   showScreenShares?: boolean;
+  /**
+   * The grid shows all room participants. If only a subset of the participants
+   * should be visible, they can be passed here.
+   */
+  participants?: Array<Participant>;
   // TODO maxVisibleParticipants
 }
 
+/**
+ * The GridView component displays the nested participants in a grid where every participants has the same size.
+ *
+ * @example
+ * ```tsx
+ * import { DefaultRoomView } from '@livekit/components-react';
+ *
+ * <LiveKitRoom>
+ *   <DefaultRoomView />
+ * <LiveKitRoom>
+ * ```
+ */
 export function GridView({ participants, showScreenShares, ...props }: GridViewProps) {
   const elementProps = mergeProps(props, { className: 'lk-participant-grid-view' });
   const filter = (ps: Array<Participant>) => participants ?? ps;

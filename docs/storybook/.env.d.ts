3bb57b1ab6aa5ba379bc3740ac2c6f912a8469a0
Jonas Schell <jonas@livekit.io>
2022-11-29 15:15:37 +0100

    add types for env vars

---
+++ docs/storybook/.env.d.ts
@@ -0,0 +1,12 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_APP_TITLE: string;
+  readonly VITE_PUBLIC_LK_SERVER_URL: string;
+  readonly VITE_PUBLIC_TEST_TOKEN: string;
+  // more env variables...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}

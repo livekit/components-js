85e58bdee757722cd3624704bc4967a9a1c4e29f
Jonas Schell <jonas@livekit.io>
2022-12-06 17:01:53 +0100

    cleanup storybook files

--- docs/storybook/.storybook/LiveKitStorybookContexts.tsx
+++
@@ -1,9 +0,0 @@
-import { LiveKitRoom, ParticipantsLoop } from '@livekit/components-react';
-import { DecoratorFn } from '@storybook/react';
-import { Room } from 'livekit-client';
-
-export type RoomContextSettings = Partial<{
-  audio: boolean;
-  video: boolean;
-  connect: boolean;
-}>;

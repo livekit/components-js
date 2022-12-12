f4e7434ab899c6b4d52d9ddea7ba341f3327932a
Jonas Schell <jonas@livekit.io>
2022-12-12 16:49:54 +0100

    fix eslint warning

--- docs/storybook/stories/participant-components/ParticipantView.stories.tsx
+++ docs/storybook/stories/participant-components/ParticipantView.stories.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import { StoryObj } from '@storybook/react';
-
 import { ParticipantView, ParticipantViewProps } from '@livekit/components-react';
-import { LkParticipantContext, LkRoomContext } from '../../.storybook/lk-decorators';
+import { StoryObj } from '@storybook/react';
 import { Track } from 'livekit-client';
+import React from 'react';
+import { LkParticipantContext, LkRoomContext } from '../../.storybook/lk-decorators';
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
   component: ParticipantView,
   decorators: [LkParticipantContext, LkRoomContext],

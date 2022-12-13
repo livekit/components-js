85de4df8f218945a201556bfe1dc41de44f9e44a
Jonas Schell <jonas@livekit.io>
2022-12-13 11:44:53 +0100

    small change

--- examples/nextjs/pages/minimal.tsx
+++ examples/nextjs/pages/minimal.tsx
@@ -4,7 +4,7 @@ import type { NextPage } from 'next';
 const MinimalExample: NextPage = () => {
   const params = typeof window !== 'undefined' ? new URLSearchParams(location.search) : null;
   const roomName = params?.get('room') ?? 'test-room';
-  const userIdentity = params?.get('user') ?? 'test-user';
+  const userIdentity = params?.get('user') ?? 'test-identity';
 
   const token = useToken({
     tokenEndpoint: process.env.NEXT_PUBLIC_LK_TOKEN_ENDPOINT,

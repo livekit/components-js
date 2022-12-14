ac5298981d8d7a59be1ecf643160c06ef2d39782
Mark Otto <markdotto@gmail.com>
2022-12-14 14:37:10 -0800

    Icon fixes
    - Ensure icons are same canvas size to prevent misalignment and shifting
    - Use currentColor for fill to allow easy customization via color or fill style rules

--- packages/react/src/icons/CameraIcon.tsx
+++ packages/react/src/icons/CameraIcon.tsx
@@ -1,11 +1,9 @@
 import * as React from 'react';
 import { SVGProps } from 'react';
 const SvgCameraIcon = (props: SVGProps<SVGSVGElement>) => (
-  <svg width={16} height={10} fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
-    <path
-      d="M0 1.5A1.5 1.5 0 0 1 1.5 0h8A1.5 1.5 0 0 1 11 1.5v7A1.5 1.5 0 0 1 9.5 10h-8A1.5 1.5 0 0 1 0 8.5v-7ZM15.2.6l-2.8 2.1a1 1 0 0 0-.4.8v3a1 1 0 0 0 .4.8l2.8 2.1A.5.5 0 0 0 16 9V1a.5.5 0 0 0-.8-.4Z"
-      fill="#000"
-    />
+  <svg width={16} height={16} fill="currentColor" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <path d="m0 4.5c0-.82843.671573-1.5 1.5-1.5h8c.8284 0 1.5.67157 1.5 1.5v7c0 .8284-.6716 1.5-1.5 1.5h-8c-.828427 0-1.5-.6716-1.5-1.5z"/>
+    <path d="m15.2 3.6-2.8 2.1c-.2518.18885-.4.48524-.4.8v3c0 .31476.1482.6111.4.8l2.8 2.1c.3296.2472.8.012.8-.4v-8c0-.41202-.4704-.64721-.8-.4z"/>
   </svg>
 );
 export default SvgCameraIcon;
--- packages/react/src/icons/QualityGoodIcon.tsx
+++ packages/react/src/icons/QualityGoodIcon.tsx
@@ -4,11 +4,11 @@ const SvgQualityGoodIcon = (props: SVGProps<SVGSVGElement>) => (
   <svg width={16} height={10} fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
     <path
       d="M0 5.5A.5.5 0 0 1 .5 5h3a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-4ZM6 .5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-9ZM12 9.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5Z"
-      fill="#000"
+      fill="currentColor"
     />
     <path
       d="M0 5.5A.5.5 0 0 1 .5 5h3a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-4ZM6 .5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-9ZM12 9.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5Z"
-      fill="#000"
+      fill="currentColor"
     />
   </svg>
 );

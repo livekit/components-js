5551a247b019824803e573a64ccc099457e7798b
Mark Otto <markdotto@gmail.com>
2022-12-15 08:10:14 -0800

    Update source, run yarn build to test, confirmed all icons updated

--- packages/react/src/icons/CameraIcon.tsx
+++ packages/react/src/icons/CameraIcon.tsx
@@ -1,9 +1,8 @@
 import * as React from 'react';
 import { SVGProps } from 'react';
 const SvgCameraIcon = (props: SVGProps<SVGSVGElement>) => (
-  <svg width={16} height={16} fill="currentColor" xmlns="http://www.w3.org/2000/svg" {...props}>
-    <path d="m0 4.5c0-.82843.671573-1.5 1.5-1.5h8c.8284 0 1.5.67157 1.5 1.5v7c0 .8284-.6716 1.5-1.5 1.5h-8c-.828427 0-1.5-.6716-1.5-1.5z"/>
-    <path d="m15.2 3.6-2.8 2.1c-.2518.18885-.4.48524-.4.8v3c0 .31476.1482.6111.4.8l2.8 2.1c.3296.2472.8.012.8-.4v-8c0-.41202-.4704-.64721-.8-.4z"/>
+  <svg fill="currentColor" height={16} width={16} xmlns="http://www.w3.org/2000/svg" {...props}>
+    <path d="M0 4.5A1.5 1.5 0 0 1 1.5 3h8A1.5 1.5 0 0 1 11 4.5v7A1.5 1.5 0 0 1 9.5 13h-8A1.5 1.5 0 0 1 0 11.5zM15.2 3.6l-2.8 2.1a1 1 0 0 0-.4.8v3a1 1 0 0 0 .4.8l2.8 2.1a.5.5 0 0 0 .8-.4V4a.5.5 0 0 0-.8-.4z" />
   </svg>
 );
 export default SvgCameraIcon;

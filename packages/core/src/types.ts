9c5a872a40fcfa43df3147f67a331ac2dd5639f8
Jonas Schell <jonas@livekit.io>
2022-12-22 14:11:51 +0100

    add TrackParticipantPair to core types

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -1,6 +1,11 @@
-import type { Participant, Track } from 'livekit-client';
+import type { Participant, Track, TrackPublication } from 'livekit-client';
 
 export type PinState = {
   pinnedParticipant?: Participant;
   pinnedSource?: Track.Source;
+};
+
+export type TrackParticipantPair = {
+  track: TrackPublication;
+  participant: Participant;
 };

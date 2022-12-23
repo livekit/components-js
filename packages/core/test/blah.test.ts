bbf7a70eb6ff4518ae24f1a95439cfb4be1d5451
Jonas Schell <jonas@livekit.io>
2022-12-23 21:43:35 +0100

    add vitest package

--- packages/core/test/blah.test.ts
+++ packages/core/test/blah.test.ts
@@ -1,7 +1,7 @@
-import { sum } from '../src';
+import { describe, it, expect } from 'vitest';
 
 describe('blah', () => {
   it('works', () => {
-    expect(sum(1, 1)).toEqual(2);
+    expect(1 + 1).toEqual(2);
   });
 });

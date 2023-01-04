537df0c0806c441bd1e962bcc23029c71a21ddad
Lukas <mail@lukasseiler.de>
2023-01-04 18:16:32 +0100

    use device specific selector for default control bar

--- packages/react/src/prefabs/ControlBar.tsx
+++ packages/react/src/prefabs/ControlBar.tsx
@@ -4,6 +4,7 @@ import { MediaDeviceMenu } from './MediaDeviceMenu';
 import { DisconnectButton } from '../components/controls/DisconnectButton';
 import { TrackToggle } from '../components/controls/TrackToggle';
 import { StartAudio } from '../components/controls/StartAudio';
+import { MediaDeviceSelect } from '../components/controls/MediaDeviceSelect';
 
 export type ControlBarProps = React.HTMLAttributes<HTMLDivElement>;
 
@@ -26,10 +27,15 @@ export function ControlBar(props: ControlBarProps) {
   return (
     // TODO: Remove styling default styling or move to styles package.
     <div className="lk-control-bar" {...props}>
-      <TrackToggle source={Track.Source.Microphone}>Microphone</TrackToggle>
-      <TrackToggle source={Track.Source.Camera}>Camera</TrackToggle>
+      <span>
+        <TrackToggle source={Track.Source.Microphone}>Microphone</TrackToggle>
+        <MediaDeviceMenu kind="audioinput" />
+      </span>
+      <span>
+        <TrackToggle source={Track.Source.Camera}>Camera</TrackToggle>
+        <MediaDeviceMenu kind="videoinput" />
+      </span>
       <TrackToggle source={Track.Source.ScreenShare}>Share screen</TrackToggle>
-      <MediaDeviceMenu>Settings</MediaDeviceMenu>
       <DisconnectButton>Leave</DisconnectButton>
       <StartAudio label="Start Audio" />
     </div>

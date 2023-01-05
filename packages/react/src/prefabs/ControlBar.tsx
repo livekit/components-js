e888ed2927becd168e951c56acfe1488b5f80089
lukasIO <mail@lukasseiler.de>
2023-01-05 14:22:27 +0100

    Add variations to controlbar and its story (#156)
    * add variations to controlbar and its story
    
    * changeset

--- packages/react/src/prefabs/ControlBar.tsx
+++ packages/react/src/prefabs/ControlBar.tsx
@@ -5,7 +5,9 @@ import { DisconnectButton } from '../components/controls/DisconnectButton';
 import { TrackToggle } from '../components/controls/TrackToggle';
 import { StartAudio } from '../components/controls/StartAudio';
 
-export type ControlBarProps = React.HTMLAttributes<HTMLDivElement>;
+export type ControlBarProps = React.HTMLAttributes<HTMLDivElement> & {
+  variation?: 'minimal' | 'verbose' | 'textOnly';
+};
 
 /**
  * The ControlBar prefab component gives the user the basic user interface
@@ -23,18 +25,34 @@ export type ControlBarProps = React.HTMLAttributes<HTMLDivElement>;
  * ```
  */
 export function ControlBar(props: ControlBarProps) {
+  const { variation = 'verbose' } = props;
+
+  const showIcon = React.useMemo(
+    () => variation === 'minimal' || variation === 'verbose',
+    [variation],
+  );
+  const showText = React.useMemo(
+    () => variation === 'textOnly' || variation === 'verbose',
+    [variation],
+  );
   return (
     // TODO: Remove styling default styling or move to styles package.
     <div className="lk-control-bar" {...props}>
       <span>
-        <TrackToggle source={Track.Source.Microphone}>Microphone</TrackToggle>
+        <TrackToggle source={Track.Source.Microphone} showIcon={showIcon}>
+          {showText && 'Microphone'}
+        </TrackToggle>
         <MediaDeviceMenu kind="audioinput" />
       </span>
       <span>
-        <TrackToggle source={Track.Source.Camera}>Camera</TrackToggle>
+        <TrackToggle source={Track.Source.Camera} showIcon={showIcon}>
+          {showText && 'Camera'}
+        </TrackToggle>
         <MediaDeviceMenu kind="videoinput" />
       </span>
-      <TrackToggle source={Track.Source.ScreenShare}>Share screen</TrackToggle>
+      <TrackToggle source={Track.Source.ScreenShare} showIcon={showIcon}>
+        {showText && 'Share screen'}
+      </TrackToggle>
       <DisconnectButton>Leave</DisconnectButton>
       <StartAudio label="Start Audio" />
     </div>

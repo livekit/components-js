af66b41a268371ec6e4cdafa83fe62c3cc78d700
Jonas Schell <jonas@livekit.io>
2023-01-05 11:52:42 +0100

    add doc-gen target for markdown

--- tooling/docs-gen/src/build.ts
+++ tooling/docs-gen/src/build.ts
@@ -7,6 +7,7 @@ import * as docgen from 'react-docgen-typescript';
 import { ComponentDoc } from 'react-docgen-typescript';
 import mkdirp from 'mkdirp';
 import { mdxComponentTemplate, mdxHookTemplate } from './mdx-template';
+import { mdComponentTemplate, mdHookTemplate } from './md-templates';
 
 type ComponentInfo = {
   def: ComponentDoc;
@@ -17,34 +18,42 @@ type ComponentInfo = {
   mdx: string;
 };
 
+type GenInfoMarkdown = {
+  def: ComponentDoc;
+  displayName: string;
+  fileName: string;
+  exportName: string;
+  isHook: boolean;
+  md: string;
+};
+
+const TARGET: 'mdx' | 'md' = 'md';
 const globAsync = promisify(glob);
-
-// const excludedPropNames = propNames.concat(['as', 'apply', 'sx', '__css', 'css']);
-
 const rootDir = path.join(__dirname, '..', '..', '..');
 const sourcePath = path.join(rootDir, 'packages');
-const outputPath = path.join(__dirname, '..', 'dist', 'components');
-
-const basePath = path.join(__dirname, '..', 'dist');
-
+const outputPath = path.join(__dirname, '..', 'dist');
 const tsConfigPath = path.join(sourcePath, '..', 'tsconfig.json');
 
 export async function main() {
-  const componentFiles = await findComponentFiles();
+  const componentFiles = await findFiles();
   if (componentFiles.length) {
     await mkdirp(outputPath);
   }
-
   log('Parsing files for component types...');
+  log(`Generation TARGET is: ${TARGET}`);
   const parsedInfo = parseInfo(componentFiles);
-
   log('Extracting component info...');
-  const componentInfo = extractComponentInfo(parsedInfo);
-
-  log('Writing component info files...');
-  await writeComponentInfoFiles(componentInfo);
-
-  log(`Processed ${componentInfo.length} components`);
+  if (TARGET === 'md') {
+    const componentInfo = extractComponentInfoMd(parsedInfo);
+    log('Writing component info files...');
+    await writeComponentInfoFilesMd(componentInfo);
+    log(`Processed ${componentInfo.length} files.`);
+  } else if (TARGET === 'mdx') {
+    const componentInfo = extractComponentInfoMdx(parsedInfo);
+    log('Writing component info files...');
+    await writeComponentInfoFilesMdx(componentInfo);
+    log(`Processed ${componentInfo.length} files.`);
+  }
 }
 
 if (require.main === module) {
@@ -55,7 +64,7 @@ if (require.main === module) {
 /**
  * Find all TypeScript files which could contain component definitions
  */
-async function findComponentFiles() {
+async function findFiles() {
   return globAsync('**/src/**/*.@(ts|tsx)', {
     cwd: sourcePath,
     ignore: ['**/core/**', '**/node_modules/**', '**/index.ts'],
@@ -83,7 +92,7 @@ function parseInfo(filePaths: string[]) {
 /**
  * Extract meta data of component docs
  */
-function extractComponentInfo(docs: ComponentDoc[]) {
+function extractComponentInfoMdx(docs: ComponentDoc[]) {
   return docs.reduce((acc, def, _, allDefs) => {
     function createUniqueName(displayName: string) {
       const existing = acc.filter(
@@ -99,7 +108,7 @@ function extractComponentInfo(docs: ComponentDoc[]) {
     const fileName = `${exportName}.mdx`;
     const mdx = def.displayName.startsWith('use')
       ? mdxHookTemplate(def)
-      : mdxComponentTemplate(def, hasHook(def.displayName, allDefs));
+      : mdxComponentTemplate(def);
 
     acc.push({
       def,
@@ -113,19 +122,66 @@ function extractComponentInfo(docs: ComponentDoc[]) {
   }, [] as ComponentInfo[]);
 }
 
+/**
+ * Extract meta data of component docs
+ */
+function extractComponentInfoMd(docs: ComponentDoc[]) {
+  return docs.reduce((acc, def, _, allDefs) => {
+    function createUniqueName(displayName: string) {
+      const existing = acc.filter(
+        (prev) => String(prev.def.displayName).toLowerCase() === displayName.toLowerCase(),
+      );
+      if (!existing.length) {
+        return displayName;
+      }
+      return `${displayName}${existing.length}`;
+    }
+
+    const isHook = def.displayName.startsWith('use');
+    const exportName = createUniqueName(def.displayName);
+    const fileName = `${exportName}.md`;
+    const md = isHook ? mdHookTemplate(def) : mdComponentTemplate(def);
+
+    acc.push({
+      def,
+      displayName: def.displayName,
+      fileName,
+      exportName,
+      isHook,
+      md: md,
+    });
+    return acc;
+  }, [] as GenInfoMarkdown[]);
+}
+
 const hasHook = (displayName: string, allDefs: ComponentDoc[]) =>
   allDefs.some((def) => def.displayName === `use${displayName}`);
 
 /**
- * Write component info as JSON to disk
+ * Write doc files as .mdx files.
  */
-async function writeComponentInfoFiles(componentInfo: ComponentInfo[]) {
+async function writeComponentInfoFilesMdx(componentInfo: ComponentInfo[]) {
   return Promise.all(
     componentInfo.map(async (info) => {
       const componentDirPath = path.join(outputPath);
       const filePath = path.join(componentDirPath, info.fileName);
       const content = info.mdx;
       await mkdirp(componentDirPath);
+      return fs.writeFile(filePath, content);
+    }),
+  );
+}
+
+/**
+ * Write doc files as .md files.
+ */
+async function writeComponentInfoFilesMd(componentInfo: GenInfoMarkdown[]) {
+  return Promise.all(
+    componentInfo.map(async (info) => {
+      const dirPath = path.join(outputPath, info.isHook ? 'hooks' : 'components');
+      const filePath = path.join(dirPath, info.fileName);
+      const content = info.md;
+      await mkdirp(dirPath);
       return fs.writeFile(filePath, content);
     }),
   );

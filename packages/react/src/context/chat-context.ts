33f9a42ec56977c3c992b0b673403799cccdc7a9
Jonas Schell <jonas@livekit.io>
2023-01-10 18:35:53 +0100

    add toggle_chat msg to chat reducer

--- packages/react/src/context/chat-context.ts
+++ packages/react/src/context/chat-context.ts
@@ -1,7 +1,10 @@
 import { WidgetState } from '@livekit/components-core';
 import * as React from 'react';
 
-export type ChatContextAction = { msg: 'show_chat' } | { msg: 'hide_chat' };
+export type ChatContextAction =
+  | { msg: 'show_chat' }
+  | { msg: 'hide_chat' }
+  | { msg: 'toggle_chat' };
 
 export function isChatContextAction(action: { msg: string }): action is ChatContextAction {
   return (
@@ -20,6 +23,8 @@ export function chatReducer(state: WidgetState, action: ChatContextAction): Widg
     return { ...state, showChat: true };
   } else if (action.msg === 'hide_chat') {
     return { ...state, showChat: false };
+  } else if (action.msg === 'toggle_chat') {
+    return { ...state, showChat: !state.showChat };
   } else {
     return { ...state };
   }

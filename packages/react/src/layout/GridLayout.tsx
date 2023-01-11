91a53462a059da2f3f953d83fdd937460f8d0d28
lukasIO <mail@lukasseiler.de>
2023-01-11 17:58:27 +0100

    Experiment with auto grid layout (#183)
    * test out auto-grid-layout
    
    * pass-through-filter

--- packages/react/src/layout/GridLayout.tsx
+++ packages/react/src/layout/GridLayout.tsx
@@ -1,14 +1,15 @@
-import { Participant } from 'livekit-client';
 import * as React from 'react';
 import { TileLoop } from '../components/TileLoop';
+import { ParticipantFilter, useParticipants } from '../hooks';
 import { mergeProps } from '../utils';
 
 export interface GridLayoutProps extends React.HTMLAttributes<HTMLDivElement> {
   /**
    * The grid shows all room participants. If only a subset of the participants
-   * should be visible, they can be passed here.
+   * should be visible, they can be filtered.
    */
-  participants?: Array<Participant>;
+  filter?: ParticipantFilter;
+  filterDependencies?: [];
   // TODO maxVisibleParticipants
 }
 
@@ -22,10 +23,20 @@ export interface GridLayoutProps extends React.HTMLAttributes<HTMLDivElement> {
  * <LiveKitRoom>
  * ```
  */
-export function GridLayout({ participants, ...props }: GridLayoutProps) {
+export function GridLayout({ filter, filterDependencies, ...props }: GridLayoutProps) {
+  const participants = useParticipants({ filter, filterDependencies });
+  const gridEl = React.createRef<HTMLDivElement>();
+
+  React.useEffect(() => {
+    gridEl.current?.style.setProperty('--lk-p-count', participants.length.toFixed(0));
+  }, [participants, gridEl]);
   const elementProps = React.useMemo(
     () => mergeProps(props, { className: 'lk-grid-layout' }),
     [props],
   );
-  return <div {...elementProps}>{props.children ?? <TileLoop />}</div>;
+  return (
+    <div ref={gridEl} {...elementProps}>
+      {props.children ?? <TileLoop filter={filter} filterDependencies={filterDependencies} />}
+    </div>
+  );
 }

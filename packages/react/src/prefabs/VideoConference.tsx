f17b6310d9764dcda798698228770e921cffd09e
Mark Otto <markdotto@gmail.com>
2023-01-13 15:12:09 -0800

    WIP move to a drawer

--- packages/react/src/prefabs/VideoConference.tsx
+++ packages/react/src/prefabs/VideoConference.tsx
@@ -55,7 +55,7 @@ export function VideoConference({ ...props }: VideoConferenceProps) {
           )}
           <ControlBar variation={isMobile ? 'minimal' : 'verbose'} controls={{ chat: true }} />
         </div>
-        <Chat style={{ display: widgetState.showChat ? 'flex' : 'none' }} />
+        <Chat style={{ visibility: widgetState.showChat ? 'visible' : 'hidden', transform: widgetState.showChat ? 'translate3d(0, 0, 0)' : ''  }} />
       </LayoutContextProvider>
       <RoomAudioRenderer />
       <ConnectionStateToast />

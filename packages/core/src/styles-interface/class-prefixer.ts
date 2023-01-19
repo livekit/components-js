2be67d7fce3921ed96b8cff911ebd596c8e8532f
Jonas Schell <jonas@livekit.io>
2023-01-19 15:41:00 +0100

    move grid layout component styles into components dif

--- packages/core/src/styles-interface/class-prefixer.ts
+++ packages/core/src/styles-interface/class-prefixer.ts
@@ -1,16 +1,11 @@
 import type { ClassNames as ComponentClasses } from '@livekit/components-styles/dist/types/general/styles.css';
 import type { ClassNames as PrefabClasses } from '@livekit/components-styles/dist/types/general/prefabs.css';
-import type { ClassNames as LayoutClasses } from '@livekit/components-styles/dist/types/general/layout.css';
 import type { UnprefixedClassNames as ComponentNoPrefixClasses } from '@livekit/components-styles/dist/types_unprefixed/styles.scss';
 import type { UnprefixedClassNames as PrefabNoPrefixClasses } from '@livekit/components-styles/dist/types_unprefixed/prefabs.scss';
-import type { UnprefixedClassNames as LayoutNoPrefixClasses } from '@livekit/components-styles/dist/types_unprefixed/layout.scss';
 import { cssPrefix } from './../constants';
 
-type UnprefixedClassNames =
-  | ComponentNoPrefixClasses
-  | PrefabNoPrefixClasses
-  | LayoutNoPrefixClasses;
-type Classes = PrefabClasses | ComponentClasses | LayoutClasses;
+type UnprefixedClassNames = ComponentNoPrefixClasses | PrefabNoPrefixClasses;
+type Classes = PrefabClasses | ComponentClasses;
 
 /**
  * This function is a type safe way to add a prefix to a HTML class attribute.

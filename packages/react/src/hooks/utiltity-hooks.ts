9d05d978df4d73cd47987e3307b47d9b830302da
Lukas <mail@lukasseiler.de>
2023-01-20 17:32:41 +0100

    Remove ResizeObserver polyfill dependency

--- packages/react/src/hooks/utiltity-hooks.ts
+++ packages/react/src/hooks/utiltity-hooks.ts
@@ -1,5 +1,6 @@
 // Utility hooks are not meant to be exposed to the user, that's why this file doesn't get included in hooks/index.tsx
-import { useEffect, useState } from 'react';
+import * as React from 'react';
+import useLatest from '@react-hook/latest';
 
 /**
  * Implementation used from https://github.com/juliencrn/usehooks-ts
@@ -13,13 +14,13 @@ export function useMediaQuery(query: string): boolean {
     return false;
   };
 
-  const [matches, setMatches] = useState<boolean>(getMatches(query));
+  const [matches, setMatches] = React.useState<boolean>(getMatches(query));
 
   function handleChange() {
     setMatches(getMatches(query));
   }
 
-  useEffect(() => {
+  React.useEffect(() => {
     const matchMedia = window.matchMedia(query);
 
     // Triggered at the first client-side load and if query changes
@@ -44,3 +45,96 @@ export function useMediaQuery(query: string): boolean {
 
   return matches;
 }
+
+/* eslint-disable no-return-assign */
+/* eslint-disable no-underscore-dangle */
+
+/**
+ * A React hook that fires a callback whenever ResizeObserver detects a change to its size
+ * code extracted from https://github.com/jaredLunde/react-hook/blob/master/packages/resize-observer/src/index.tsx in order to not include the polyfill for resize-observer
+ *
+ *   the `target` resizes
+ */
+export function useResizeObserver<T extends HTMLElement>(
+  target: React.RefObject<T> | T | null,
+  callback: UseResizeObserverCallback,
+) {
+  const resizeObserver = getResizeObserver();
+  const storedCallback = useLatest(callback);
+
+  React.useLayoutEffect(() => {
+    let didUnsubscribe = false;
+    const targetEl = target && 'current' in target ? target.current : target;
+    if (!targetEl) return;
+
+    function cb(entry: ResizeObserverEntry, observer: ResizeObserver) {
+      if (didUnsubscribe) return;
+      storedCallback.current(entry, observer);
+    }
+
+    resizeObserver.subscribe(targetEl as HTMLElement, cb);
+
+    return () => {
+      didUnsubscribe = true;
+      resizeObserver.unsubscribe(targetEl as HTMLElement, cb);
+    };
+  }, [target, resizeObserver, storedCallback]);
+
+  return resizeObserver.observer;
+}
+
+function createResizeObserver() {
+  let ticking = false;
+  let allEntries: ResizeObserverEntry[] = [];
+
+  const callbacks: Map<unknown, Array<UseResizeObserverCallback>> = new Map();
+
+  const observer = new ResizeObserver((entries: ResizeObserverEntry[], obs: ResizeObserver) => {
+    allEntries = allEntries.concat(entries);
+    if (!ticking) {
+      window.requestAnimationFrame(() => {
+        const triggered = new Set<Element>();
+        for (let i = 0; i < allEntries.length; i++) {
+          if (triggered.has(allEntries[i].target)) continue;
+          triggered.add(allEntries[i].target);
+          const cbs = callbacks.get(allEntries[i].target);
+          cbs?.forEach((cb) => cb(allEntries[i], obs));
+        }
+        allEntries = [];
+        ticking = false;
+      });
+    }
+    ticking = true;
+  });
+
+  return {
+    observer,
+    subscribe(target: HTMLElement, callback: UseResizeObserverCallback) {
+      observer.observe(target);
+      const cbs = callbacks.get(target) ?? [];
+      cbs.push(callback);
+      callbacks.set(target, cbs);
+    },
+    unsubscribe(target: HTMLElement, callback: UseResizeObserverCallback) {
+      const cbs = callbacks.get(target) ?? [];
+      if (cbs.length === 1) {
+        observer.unobserve(target);
+        callbacks.delete(target);
+        return;
+      }
+      const cbIndex = cbs.indexOf(callback);
+      if (cbIndex !== -1) cbs.splice(cbIndex, 1);
+      callbacks.set(target, cbs);
+    },
+  };
+}
+
+let _resizeObserver: ReturnType<typeof createResizeObserver>;
+
+const getResizeObserver = () =>
+  !_resizeObserver ? (_resizeObserver = createResizeObserver()) : _resizeObserver;
+
+export type UseResizeObserverCallback = (
+  entry: ResizeObserverEntry,
+  observer: ResizeObserver,
+) => unknown;

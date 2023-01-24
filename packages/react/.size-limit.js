336613ae9e0505d9b366d4bc9315acee80f607e0
Lukas <mail@lukasseiler.de>
2023-01-24 15:46:29 +0100

    include core in size calculation

--- packages/react/.size-limit.js
+++ packages/react/.size-limit.js
@@ -4,10 +4,10 @@ module.exports = [
     path: 'dist/index.mjs',
     import: '{ LiveKitRoom }',
     limit: '10 kB',
-    ignore: ['@livekit/components-core', 'livekit-client', 'react', 'react-dom'],
+    ignore: ['livekit-client', 'react', 'react-dom'],
   },
   {
-    name: 'Room with defaults and core',
+    name: 'Room with defaults',
     path: 'dist/index.mjs',
     import: '{ LiveKitRoom, VideoConference }',
     limit: '50 kB',

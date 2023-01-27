4fc3596549ed7b49ffe9927bd39ad2722c2e6ad1
Lukas <mail@lukasseiler.de>
2023-01-27 15:16:57 +0100

    add data channel hook to react package

---
+++ packages/react/src/hooks/dataChannel-hooks.ts
@@ -0,0 +1,25 @@
+import { BaseDataMessage, setupDataMessageHandler } from '@livekit/components-core';
+import * as React from 'react';
+import { useRoomContext } from '../context';
+import { useObservableState } from '../helper';
+
+export interface DataChannelMessageProps<T extends BaseDataMessage> {
+  type: T['type'];
+}
+
+export function useDataChannelMessages<T extends BaseDataMessage>(
+  props: DataChannelMessageProps<T>,
+) {
+  const room = useRoomContext();
+  const { send, messageObservable } = React.useMemo(
+    () => setupDataMessageHandler(room, props.type),
+    [room, props.type],
+  );
+
+  const message = useObservableState(messageObservable, undefined);
+
+  return {
+    message,
+    send,
+  };
+}

66c6473afe1687031f9155609298ab677909caba
Jonas Schell <jonas@livekit.io>
2023-02-09 17:26:43 +0100

    add interactive example performance tests

--- packages/react/test/performance/HelloWord.test.tsx
+++ packages/react/test/performance/HelloWord.test.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { describe, it } from 'vitest';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { Profiler } from 'react';
 
 function Row(id, phase, actualDuration, baseDuration) {
@@ -13,14 +14,22 @@ function Row(id, phase, actualDuration, baseDuration) {
 /**
  * @see https://beta.reactjs.org/reference/react/Profiler#onrender-parameters
  */
-const onRender: React.ProfilerOnRenderCallback = (id, phase, actualDuration, baseDuration) => {
-  const table = new Row(id, phase, actualDuration, baseDuration);
+function onRender(
+  id: string,
+  phase: 'mount' | 'update',
+  actualDuration: number,
+  baseDuration: number,
+): typeof Row {
+  const row = new Row(id, phase, actualDuration, baseDuration);
+  return row;
+}
 
-  console.table([table]);
-};
+function printTable(rows) {
+  console.table(rows);
+}
 
 describe('Basic performance test setup', () => {
-  it('Test render time', () => {
+  it('Test render time with no interaction', () => {
     render(
       <Profiler id="hello-world" onRender={onRender}>
         {Array.from(new Array(1)).map((e, i) => (
@@ -30,5 +39,32 @@ describe('Basic performance test setup', () => {
     );
 
     screen.debug();
+  });
+  it.only('Test render time with button click', async () => {
+    const logs: (typeof Row)[] = [];
+    const Component = () => {
+      const [state, setState] = React.useState(0);
+      return <button onClick={() => setState((value) => value + 1)}>Button</button>;
+    };
+    render(
+      <Profiler
+        id="click"
+        onRender={(id, phase, actualDuration, baseDuration) =>
+          logs.push(onRender(id, phase, actualDuration, baseDuration))
+        }
+      >
+        <Component />
+      </Profiler>,
+    );
+
+    const button = screen.getByText('Button');
+    await userEvent.click(button);
+    await userEvent.click(button);
+    await userEvent.click(button);
+    await userEvent.click(button);
+    await userEvent.click(button);
+    await userEvent.click(button);
+    await userEvent.click(button);
+    printTable(logs);
   });
 });

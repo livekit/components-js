a5bf0638463ffae92661c19a6aec251543281f36
Jonas Schell <jonas@livekit.io>
2023-02-21 16:54:40 +0100

    add docstring

--- packages/react/test/env.ts
+++ packages/react/test/env.ts
@@ -1,1 +1,7 @@
+/**
+ * Environment variables related exclusively to tests.
+ */
+
+/** If set to true skip all performance tests that are not
+ * "normal" test but rather performance investigations. */
 export const SKIP_PERFORMANCE_TESTS = false;

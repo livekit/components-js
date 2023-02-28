50f10ed97865df13fbbb9cd123d4b49d18c43976
Jonas Schell <jonas@livekit.io>
2023-02-28 17:12:27 +0100

    add temp test page to examples

---
+++ examples/nextjs/pages/placeholder.tsx
@@ -0,0 +1,43 @@
+import {
+  ControlBar,
+  LiveKitRoom,
+  TrackTileLoop,
+  useToken,
+  VideoConference,
+} from '@livekit/components-react';
+import { Track } from 'livekit-client';
+import type { NextPage } from 'next';
+
+const Placeholder: NextPage = () => {
+  const params = typeof window !== 'undefined' ? new URLSearchParams(location.search) : null;
+  const roomName = params?.get('room') ?? 'test-room';
+  const userIdentity = params?.get('user') ?? 'test-identity';
+
+  const token = useToken(process.env.NEXT_PUBLIC_LK_TOKEN_ENDPOINT, roomName, {
+    userInfo: {
+      identity: userIdentity,
+      name: userIdentity,
+    },
+  });
+
+  return (
+    <div style={{ padding: '2rem' }}>
+      <LiveKitRoom
+        video={true}
+        audio={true}
+        token={token}
+        serverUrl={process.env.NEXT_PUBLIC_LK_SERVER_URL}
+      >
+        <TrackTileLoop
+          sources={[
+            { source: Track.Source.Camera, withPlaceholder: true },
+            { source: Track.Source.ScreenShare, withPlaceholder: true },
+          ]}
+        ></TrackTileLoop>
+        <ControlBar />
+      </LiveKitRoom>
+    </div>
+  );
+};
+
+export default Placeholder;

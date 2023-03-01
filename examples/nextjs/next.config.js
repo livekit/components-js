118698cabb4a5350cef96eadb159e145e2e787e1
lukasIO <mail@lukasseiler.de>
2023-03-01 12:15:26 +0100

    load source maps for .mjs files

--- examples/nextjs/next.config.js
+++ examples/nextjs/next.config.js
@@ -1,7 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  productionBrowserSourceMaps: true,
   swcMinify: true,
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }) => {
+    // Important: return the modified config
+    config.module.rules = [
+      ...config.module.rules,
+      {
+        test: /\.mjs$/,
+        enforce: 'pre',
+        use: ['source-map-loader'],
+      },
+    ];
+    return config;
+  },
 };
 
 module.exports = nextConfig;

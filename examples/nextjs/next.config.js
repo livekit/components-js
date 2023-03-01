7aaf0afe7ce04b11212c64b63094274e6cb8fa4b
lukasIO <mail@lukasseiler.de>
2023-03-01 12:21:42 +0100

    remove debug changes

--- examples/nextjs/next.config.js
+++ examples/nextjs/next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  productionBrowserSourceMaps: true,
   swcMinify: true,
   webpack: (config, { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }) => {
     // Important: return the modified config

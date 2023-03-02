58c0090989022b95131df80ec3363f2002d2394c
lukasIO <mail@lukasseiler.de>
2023-03-02 16:10:03 +0100

    Check for actually selected device after switching in mediaDeviceSelect (#335)

--- packages/core/src/components/mediaDeviceSelect.ts
+++ packages/core/src/components/mediaDeviceSelect.ts
@@ -1,4 +1,4 @@
-import { LocalAudioTrack, LocalVideoTrack, Room } from 'livekit-client';
+import { LocalAudioTrack, LocalVideoTrack, Room, Track } from 'livekit-client';
 import { BehaviorSubject, map, mergeWith } from 'rxjs';
 import log from '../logger';
 import { observeParticipantMedia } from '../observables/participant';
@@ -32,10 +32,24 @@ export function setupDeviceSelector(kind: MediaDeviceKind, room?: Room) {
     if (room) {
       log.debug(`Switching active device of kind "${kind}" with id ${id}.`);
       await room.switchActiveDevice(kind, id);
+      let actualDeviceId: string | undefined = id;
+      if (kind === 'videoinput') {
+        actualDeviceId = await room.localParticipant
+          .getTrack(Track.Source.Camera)
+          ?.track?.getDeviceId();
+      } else if (kind === 'audioinput') {
+        actualDeviceId = await room.localParticipant
+          .getTrack(Track.Source.Microphone)
+          ?.track?.getDeviceId();
+      }
+      if (actualDeviceId !== id && id !== 'default') {
+        log.warn(`Failed to select the desired device. Desired: ${id}. Actual: ${actualDeviceId}`);
+      }
+      activeDeviceSubject.next(id === 'default' ? id : actualDeviceId);
     } else {
       log.debug('Skip the device switch because the room object is not available. ');
+      activeDeviceSubject.next(id);
     }
-    activeDeviceSubject.next(id);
   };
   const className: string = prefixClass('media-device-select');
   return {

7af20fbbf9e45dfd3ab2ee93749e8b2adf754980
Jonas Schell <jonas@livekit.io>
2023-03-06 12:06:37 +0200

    handle dropping of items

--- packages/core/src/sorting/tile-array-update.test.ts
+++ packages/core/src/sorting/tile-array-update.test.ts
@@ -1,12 +1,11 @@
 import { describe, test, expect } from 'vitest';
-import { swapItems, visualPageChange } from './tile-array-update';
+import { divideIntoPages, swapItems, updatePages, visualPageChange } from './tile-array-update';
 
 describe('Test visualPageChange function.', () => {
   test.each([
-    // { state: [1, 2, 3, 4, 5], next: [1, 2, 3, 4, 5], expected: [] },
-    // { state: [], next: [], expected: [] },
-    // { state: [1, 2], next: [2, 1], expected: [] },
-    // { state: [1, 2, 3], next: [2, 3, 1], expected: [] },
+    { state: [], next: [], expected: { dropped: [], added: [] } },
+    { state: [1, 2, 3], next: [1, 2, 3], expected: { dropped: [], added: [] } },
+    { state: [1, 2], next: [2, 1], expected: { dropped: [], added: [] } },
   ])('Arrays with the same members should not tigger a update.', ({ state, next, expected }) => {
     const result = visualPageChange(state, next);
     expect(result).toStrictEqual(expected);
@@ -32,6 +31,71 @@ describe('Test swapping items', () => {
     { moveForward: 4, moveBack: 2, list: [1, 2, 3, 4, 5], expected: [1, 4, 3, 2, 5] },
   ])('', ({ moveForward, moveBack, list, expected }) => {
     const result = swapItems<number>(moveForward, moveBack, list);
+    expect(result).toStrictEqual(expected);
+  });
+});
+
+describe('Test dividing list into pages.', () => {
+  test.each([
+    {
+      state: [1, 2, 3, 4, 5, 6],
+      itemsOnPage: 3,
+      expected: [
+        [1, 2, 3],
+        [4, 5, 6],
+      ],
+    },
+    {
+      state: [1, 2, 3, 4, 5, 6],
+      itemsOnPage: 2,
+      expected: [
+        [1, 2],
+        [3, 4],
+        [5, 6],
+      ],
+    },
+    {
+      state: [1, 2, 3, 4, 5, 6],
+      itemsOnPage: 4,
+      expected: [
+        [1, 2, 3, 4],
+        [5, 6],
+      ],
+    },
+  ])('', ({ state, itemsOnPage, expected }) => {
+    const result = divideIntoPages(state, itemsOnPage);
+    expect(result).toStrictEqual(expected);
+  });
+});
+
+describe.only('Test updating pages only if needed', () => {
+  test.each([
+    {
+      state: [1, 2, 3, 4, 5, 6],
+      next: [2, 1, 3, 4, 5, 6],
+      expected: [1, 2, 3, 4, 5, 6],
+      itemsOnPage: 3,
+    },
+    {
+      state: [1, 2, 3, 4, 5, 6],
+      next: [6, 2, 3, 4, 5, 1],
+      expected: [6, 2, 3, 4, 5, 1],
+      itemsOnPage: 3,
+    },
+    {
+      state: [1, 2, 3, 4],
+      next: [1, 2, 3],
+      expected: [1, 2, 3],
+      itemsOnPage: 2,
+    },
+    // {
+    //   state: [1, 2, 3],
+    //   next: [1, 2, 3, 4],
+    //   expected: [1, 2, 3, 4],
+    //   itemsOnPage: 2,
+    // },
+  ])('', ({ state, next, itemsOnPage, expected }) => {
+    const result = updatePages<number>(state, next, itemsOnPage);
     expect(result).toStrictEqual(expected);
   });
 });
--- packages/core/src/sorting/tile-array-update.ts
+++ packages/core/src/sorting/tile-array-update.ts
@@ -10,6 +10,7 @@
 // import { Track } from 'livekit-client';
 import { difference } from '../helper';
 import { isTrackBundle, TrackBundleWithPlaceholder } from '../types';
+import { chunk, zip } from 'lodash';
 
 type Changes<T> = {
   dropped: T[];
@@ -23,6 +24,8 @@ export function visualPageChange<T>(state: T[], next: T[]): Changes<T> {
   //       `This function can only compare arrays with the same number of elements. Got arrays with length: ${state.length} !== ${next.length}.`,
   //     );
   //   }
+
+  //TDOO: probably need to update this when switching to TrackBundles
   const stateSet = new Set(state);
   const nextSet = new Set(next);
   const droppedMembers = Array.from(difference(stateSet, nextSet));
@@ -32,6 +35,10 @@ export function visualPageChange<T>(state: T[], next: T[]): Changes<T> {
     dropped: droppedMembers,
     added: addedMembers,
   };
+}
+
+function listNeedsUpdating<T>(changes: Changes<T>): boolean {
+  return changes.added.length !== 0 || changes.dropped.length !== 0;
 }
 
 export function findTrackBundleIndex(
@@ -63,4 +70,65 @@ export function swapItems<T>(moveForward: T, moveBack: T, trackBundles: T[]): T[
   trackBundles.splice(indexReplaceWith, 1, moveForward);
 
   return trackBundles;
+}
+
+export function dropItem<T>(itemToDrop: T, list: T[]): T[] {
+  //TODO: const indexOfElementToDrop = findTrackBundleIndex(itemToDrop, list);
+  const indexOfElementToDrop = list.findIndex((item) => item === itemToDrop, list);
+  list.splice(indexOfElementToDrop, 1);
+  return list;
+}
+
+export function divideIntoPages<T>(elements: T[], maxElementsOnPage: number): Array<T[]> {
+  const pages = chunk(elements, maxElementsOnPage);
+  return pages;
+}
+
+/** Divide the list of elements into pages and and check if pages need updating. */
+export function updatePages<T>(currentList: T[], nextList: T[], maxItemsOnPage: number): T[] {
+  let updatedList = [...currentList];
+  const currentPages = divideIntoPages(currentList, maxItemsOnPage);
+  const nextPages = divideIntoPages(nextList, maxItemsOnPage);
+
+  zip(currentPages, nextPages).forEach(([currentPage, nextPage], pageIndex) => {
+    if (currentPage && nextPage) {
+      // 1) Identify  missing tile.
+      const updatedPage = divideIntoPages(updatedList, maxItemsOnPage)[pageIndex];
+      const changes = visualPageChange(updatedPage, nextPage);
+
+      if (listNeedsUpdating(changes)) {
+        console.log(
+          `Detected visual changes on page: ${pageIndex}, current: ${currentPage}, next: ${nextPage}`,
+          { changes },
+        );
+        // ## Swap Items
+        if (changes.added.length === changes.dropped.length) {
+          console.log(
+            `Additions and removal are equal working with swaps: ${changes.added.length} === ${changes.dropped.length}`,
+          );
+          zip(changes.added, changes.dropped).forEach(([added, dropped]) => {
+            if (added && dropped) {
+              console.log(`Before swap action: `, updatedList);
+              updatedList = swapItems(added, dropped, updatedList);
+              console.log(`After swap action: `, updatedList);
+            } else {
+              throw new Error(
+                `For a swap action we need a addition and a removal one is missing: ${added}, ${dropped}`,
+              );
+            }
+          });
+        }
+        // ## Handle Drop Items
+        if (changes.added.length === 0 && changes.dropped.length > 0) {
+          changes.dropped.forEach((item) => {
+            console.log(`Before drop action: `, updatedList);
+            updatedList = dropItem(item, updatedList);
+            console.log(`After drop action: `, updatedList);
+          });
+        }
+      }
+    }
+  });
+
+  return updatedList;
 }

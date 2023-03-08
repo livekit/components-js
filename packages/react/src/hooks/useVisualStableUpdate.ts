b6fa5c6e93b2c630e8f6316620e41b27040c63a7
Jonas Schell <jonas@livekit.io>
2023-03-08 13:58:07 +0200

    move visualStableUpdate into extra hook

---
+++ packages/react/src/hooks/useVisualStableUpdate.ts
@@ -0,0 +1,36 @@
+import {
+  sortTrackBundles,
+  TrackBundleWithPlaceholder,
+  updatePages,
+} from '@livekit/components-core';
+import * as React from 'react';
+
+/**
+ * The useVisualStableUpdate hook tries to keep visual updates of the TackBundles array to a minimum,
+ * while still trying to display important tiles such as speaking participants or screen shares.
+ *
+ * Updating works with pagination. For example, if a participant starts speaking on the second page,
+ * they will be moved to the first page by replacing the least active/interesting participant on the first page.
+ */
+export function useVisualStableUpdate(
+  /** `TrackBundle`s to display in the grid.  */
+  trackBundles: TrackBundleWithPlaceholder[],
+  maxItemsOnPage: number,
+): { trackBundles: TrackBundleWithPlaceholder[] } {
+  const stateTrackBundles = React.useRef<TrackBundleWithPlaceholder[]>([]);
+  const maxTilesOnPage = React.useRef<number>(-1);
+
+  const nextSortedTrackBundles = sortTrackBundles(trackBundles);
+  const updatedTrackBundles =
+    maxItemsOnPage !== maxTilesOnPage.current
+      ? nextSortedTrackBundles
+      : updatePages(stateTrackBundles.current, nextSortedTrackBundles, maxItemsOnPage);
+
+  // Save info for next render to update with minimal visual change.
+  stateTrackBundles.current = trackBundles;
+  maxTilesOnPage.current = maxItemsOnPage;
+
+  return {
+    trackBundles: updatedTrackBundles,
+  };
+}

80700399d64b5472d93848a196d7aad12be813e3
Jonas Schell <jonas@livekit.io>
2023-03-14 14:59:09 +0200

    refactor all participant-hooks into separate files

--- packages/react/src/hooks/index.ts
+++ packages/react/src/hooks/index.ts
@@ -1,9 +1,21 @@
-export * from './participant-hooks';
-export { useToken, UseTokenOptions } from './useToken';
 export { useDataChannel } from './useDataChannel';
-export { useTracks } from './useTracks';
 export { useGridLayout } from './useGridLayout';
-export { useVisualStableUpdate } from './useVisualStableUpdate';
-export { usePagination } from './usePagination';
+export { useIsMuted, UseIsMutedOptions } from './useIsMuted';
+export { useIsSpeaking } from './useIsSpeaking';
+export { useLocalParticipant } from './useLocalParticipant';
+export { useLocalParticipantPermissions } from './useLocalParticipantPermissions';
 export { useMediaTrack } from './useMediaTrack';
 export { useMediaTrackByName } from './useMediaTrackByName';
+export { usePagination } from './usePagination';
+export {
+  useParticipantPermissions,
+  UseParticipantPermissionsOptions,
+} from './useParticipantPermissions';
+export { useParticipants, UseParticipantsOptions } from './useParticipants';
+export { useRemoteParticipant, UseRemoteParticipantOptions } from './useRemoteParticipant';
+export { useRemoteParticipants, UseRemoteParticipantsOptions } from './useRemoteParticipants';
+export { useSortedParticipants } from './useSortedParticipants';
+export { useSpeakingParticipants } from './useSpeakingParticipants';
+export { useToken, UseTokenOptions } from './useToken';
+export { useTracks } from './useTracks';
+export { useVisualStableUpdate } from './useVisualStableUpdate';

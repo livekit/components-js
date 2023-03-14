d4790a4be662e78ca12e225d2d1d39398fcf524e
Jonas Schell <jonas@livekit.io>
2023-03-14 14:22:59 +0200

    rename useDataChannel hook file

--- packages/react/src/hooks/index.ts
+++ packages/react/src/hooks/index.ts
@@ -1,7 +1,7 @@
 export * from './participant-hooks';
 export * from './token-hooks';
 export * from './track-hooks';
-export * from './dataChannel-hooks';
+export { useDataChannel } from './useDataChannel';
 export { useTracks } from './useTracks';
 export { useGridLayout } from './useGridLayout';
 export { useVisualStableUpdate } from './useVisualStableUpdate';

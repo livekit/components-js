164a0cedab03d2cae9d7c56d701b43289bfe5b14
Jonas Schell <jonas@livekit.io>
2023-03-17 11:37:34 +0200

    mark function a internal

--- packages/core/src/observables/dom-event.ts
+++ packages/core/src/observables/dom-event.ts
@@ -12,6 +12,8 @@ import {
 /**
  * Returns true if the user is interacting with the HTML element,
  * and returns false if there is no interaction for a specified period of time.
+ *
+ * @internal
  */
 export function createInteractingObservable(htmlElement: HTMLElement | null, inactiveAfter = 1000) {
   if (htmlElement === null) return of(false);

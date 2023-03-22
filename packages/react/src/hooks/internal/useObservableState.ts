a916027189a8575b91992eebe4baf2e8b3c808d2
lukasIO <mail@lukasseiler.de>
2023-03-22 12:02:41 +0100

    avoid additional rxjs imports to keep bundle size down

--- packages/react/src/hooks/internal/useObservableState.ts
+++ packages/react/src/hooks/internal/useObservableState.ts
@@ -4,11 +4,11 @@ import { Observable } from 'rxjs';
 /**
  * @internal
  */
-export function useObservableState<T>(observable: Observable<T>, startWith: T) {
+export function useObservableState<T>(observable: Observable<T> | undefined, startWith: T) {
   const [state, setState] = React.useState<T>(startWith);
   React.useEffect(() => {
     // observable state doesn't run in SSR
-    if (typeof window === 'undefined') return;
+    if (typeof window === 'undefined' || !observable) return;
     const subscription = observable.subscribe(setState);
     return () => subscription.unsubscribe();
   }, [observable]);

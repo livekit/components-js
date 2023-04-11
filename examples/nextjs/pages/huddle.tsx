6f69b7ce00b17a425b100861789ed0c52b8ffae7
Jonas Schell <jonas@livekit.io>
2023-04-11 17:13:49 +0200

    update huddle example

--- examples/nextjs/pages/huddle.tsx
+++ examples/nextjs/pages/huddle.tsx
@@ -1,26 +1,26 @@
-import { PinState } from '@livekit/components-core';
+import { PinState, TrackReference, TrackReferenceOrPlaceholder } from '@livekit/components-core';
 import {
   DisconnectButton,
   LiveKitRoom,
   LocalUserChoices,
   TrackToggle,
   ParticipantName,
-  ParticipantLoop,
   ParticipantTile,
   LayoutContextProvider,
   PreJoin,
   RoomAudioRenderer,
   StartAudio,
   useConnectionState,
-  useParticipantContext,
   useParticipants,
-  useLayoutContext,
   useScreenShare,
   useToken,
   ClearPinButton,
   MediaDeviceMenu,
-  FocusLayout,
   VideoTrack,
+  GridLayout,
+  useTracks,
+  TrackContext,
+  LayoutContext,
 } from '@livekit/components-react';
 import { ConnectionState, Room, Track, TrackPublication } from 'livekit-client';
 
@@ -30,11 +30,8 @@ import styles from '../styles/Huddle.module.scss';
 
 const Huddle: NextPage = () => {
   const params = typeof window !== 'undefined' ? new URLSearchParams(location.search) : null;
-
   const [roomName] = useState(params?.get('room') || 'test-room');
-
   const [preJoinChoices, setPreJoinChoices] = useState<LocalUserChoices | undefined>(undefined);
-
   return (
     <main className={styles.main} data-lk-theme="default">
       {preJoinChoices ? (
@@ -49,118 +46,54 @@ const Huddle: NextPage = () => {
   );
 };
 
-const CustomGridLayout = ({ room }: { room?: Room }) => {
-  const { screenShareTrack, allScreenShares } = useScreenShare({ room });
-  const participants = useParticipants({ updateOnlyOn: [] });
+const CustomGridLayout = () => {
   const gridContainerRef = useRef(null);
-
-  const props = useMemo(() => {
-    const cssProperties = {};
-    console.log(`Adjust layout for ${participants.length}`);
-    const length = participants.length;
-
-    if (length <= 20) {
-      // @ts-ignore
-      cssProperties['--participant-size'] = '15rem';
-    }
-    if (length <= 10) {
-      // @ts-ignore
-      cssProperties['--participant-size'] = '16rem';
-    }
-    if (length <= 2) {
-      // @ts-ignore
-      cssProperties['--participant-size'] = '24rem';
-    }
-
-    return cssProperties;
-  }, [participants.length]);
-
+  const tracks = useTracks([
+    { source: Track.Source.Camera, withPlaceholder: true },
+    { source: Track.Source.ScreenShare, withPlaceholder: false },
+  ]);
   return (
-    <div ref={gridContainerRef} className={styles.gridLayout} style={props}>
-      <ParticipantLoop>
-        <CustomParticipantTile />
-      </ParticipantLoop>
-      <ParticipantLoop
-        filter={(participant) => participant.isScreenShareEnabled}
-        filterDependencies={[screenShareTrack, allScreenShares]}
-      >
-        <CustomScreenShareView />
-      </ParticipantLoop>
+    <div ref={gridContainerRef} className={styles.gridLayout}>
+      <GridLayout tracks={tracks} style={{ width: 'auto', height: 'auto' }}>
+        <TrackContext.Consumer>
+          {(track) => track && <CustomParticipantTile trackRef={track} />}
+        </TrackContext.Consumer>
+      </GridLayout>
     </div>
   );
 };
 
-const CustomFocusLayout = ({
-  screenShareTrack,
-}: {
-  screenShareTrack: TrackPublication | undefined;
-}) => {
-  // const { state: focusState } = useLayoutContext().pin;
+const CustomFocusLayout = ({ track }: { track: TrackReferenceOrPlaceholder }) => {
   return (
     <div className={styles.focusLayout}>
       <div className={styles.screenShareContainer}>
-        <CustomFocus></CustomFocus>
+        <CustomParticipantTile trackRef={track} />
       </div>
-      <aside>
-        <section>
-          {/* <ParticipantLoop
-            filter={(participant) =>
-              !isParticipantTrackInFocus(participant, focusState, Track.Source.Camera)
-            }
-            filterDependencies={[screenShareTrack, focusState]}
-          >
-            <CustomParticipantTile />
-          </ParticipantLoop>
-          <ParticipantLoop
-            filter={(participant) =>
-              participant.isScreenShareEnabled &&
-              !isParticipantTrackInFocus(participant, focusState, Track.Source.Camera)
-            }
-            filterDependencies={[screenShareTrack, focusState]}
-          >
-            <CustomScreenShareView />
-          </ParticipantLoop> */}
-        </section>
-      </aside>
     </div>
   );
 };
 
-const CustomFocus = () => {
-  const { state } = useLayoutContext().pin;
+interface CustomParticipantTileProps {
+  trackRef: TrackReferenceOrPlaceholder;
+}
 
-  return <FocusLayout></FocusLayout>;
-};
-
-const CustomScreenShareView = () => {
-  const participant = useParticipantContext();
-
+const CustomParticipantTile = ({ trackRef }: CustomParticipantTileProps) => {
   return (
-    <ParticipantTile
-      participant={participant}
-      className={styles.participantView}
-      source={Track.Source.ScreenShare}
-    >
-      <VideoTrack source={Track.Source.ScreenShare} className={styles.video}></VideoTrack>
-      <div className={styles.screenShareBanner}>
-        <ParticipantName style={{ display: 'inline' }} />
-        <span>Screen share</span>
-      </div>
-    </ParticipantTile>
-  );
-};
-
-const CustomParticipantTile = () => {
-  const participant = useParticipantContext();
-  return (
-    <ParticipantTile participant={participant} className={styles.participantView}>
-      <VideoTrack source={Track.Source.Camera} className={styles.video}></VideoTrack>
+    <ParticipantTile {...trackRef} className={styles.participantView}>
+      <VideoTrack source={trackRef.source} className={styles.video}></VideoTrack>
       <div className={styles.nameContainer}>
         <img
           className={styles.nameMutedIcon}
           src="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='20' height='20' viewBox='0 0 20 20' fill='none'%3E%3Cpath d='M5.35086 10.3436C5.12445 9.77312 5 9.15109 5 8.5V8.25C5 7.83579 4.66421 7.5 4.25 7.5C3.83579 7.5 3.5 7.83579 3.5 8.25V8.5C3.5 9.60794 3.7772 10.6511 4.26603 11.564L5.35086 10.3436ZM7.62904 14.554C8.36362 14.8419 9.16335 15 10 15C13.5899 15 16.5 12.0899 16.5 8.5V8.25C16.5 7.83579 16.1642 7.5 15.75 7.5C15.3358 7.5 15 7.83579 15 8.25V8.5C15 11.2614 12.7614 13.5 10 13.5C9.55549 13.5 9.12453 13.442 8.71427 13.3331L7.62904 14.554Z' fill='currentColor'%3E%3C/path%3E%3Cpath d='M10 14.25V17' stroke='currentColor' stroke-width='1.5' stroke-linecap='round'%3E%3C/path%3E%3Cpath d='M12.25 17.25L7.75 17.25' stroke='currentColor' stroke-width='1.5' stroke-linecap='round'%3E%3C/path%3E%3Cpath d='M12.1271 2.72031C11.5376 2.2685 10.8001 2 10 2C8.067 2 6.5 3.567 6.5 5.5V8.5C6.5 8.67302 6.51255 8.84311 6.5368 9.0094L8 7.3633V5.5C8 4.39543 8.89543 3.5 10 3.5C10.4175 3.5 10.8051 3.62793 11.1258 3.84674L12.1271 2.72031ZM9.90052 11.9986C9.93357 11.9995 9.96673 12 10 12C11.933 12 13.5 10.433 13.5 8.5V7.9492L9.90052 11.9986Z' fill='currentColor'%3E%3C/path%3E%3Cpath class='p-huddle_mic_icon__microphone--slash p-huddle_mic_icon__microphone--slash_animated' d='M4 15.25L15.5 2.25' stroke='currentColor' stroke-width='1.5' stroke-linecap='round'%3E%3C/path%3E%3C/svg%3E"
         />
-        <ParticipantName className={styles.name}></ParticipantName>
+        {trackRef.source === Track.Source.ScreenShare ? (
+          <div className={styles.screenShareBanner}>
+            <ParticipantName style={{ display: 'inline' }} />
+            <span>Screen share</span>
+          </div>
+        ) : (
+          <ParticipantName className={styles.name}></ParticipantName>
+        )}
       </div>
       <svg width="100%" height="100%" viewBox="0 -10 10 10">
         <path
@@ -259,7 +192,11 @@ const HuddleRoomView = ({
               {layout === 'grid' ? (
                 <CustomGridLayout />
               ) : (
-                <CustomFocusLayout screenShareTrack={screenShareTrack}></CustomFocusLayout>
+                <LayoutContext.Consumer>
+                  {(context) =>
+                    context?.pin.state?.length && <CustomFocusLayout track={context.pin.state[0]} />
+                  }
+                </LayoutContext.Consumer>
               )}
               <div className={styles.mediaControls}>
                 <div>

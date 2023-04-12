e12768c54182b5a107936d0369b5fcc2b9bd87e8
Jonas Schell <jonas@livekit.io>
2023-04-12 15:54:18 +0200

    add pagination indicator to gird

---
+++ packages/react/src/components/controls/PaginationIndicator.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+
+export type PaginationIndicatorProps = {
+  totalPageCount: number;
+  currentPage: number;
+};
+
+export function PaginationIndicator({ totalPageCount, currentPage }: PaginationIndicatorProps) {
+  console.log('xxx', { totalPageCount }, { currentPage });
+
+  const bubbles = new Array(totalPageCount).fill('').map((_, index) => {
+    if (index + 1 === currentPage) {
+      return <span data-lk-active key={index} />;
+    } else {
+      return <span key={index} />;
+    }
+  });
+
+  return <div className="lk-pagination-indicator">{bubbles}</div>;
+}

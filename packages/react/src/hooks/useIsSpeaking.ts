8a9cc4eb796e51bbc9f1d7e3f14682077490fc8f
Jonas Schell <jonas@livekit.io>
2023-04-14 17:01:21 +0200

    update useIsSpeaking

--- packages/react/src/hooks/useIsSpeaking.ts
+++ packages/react/src/hooks/useIsSpeaking.ts
@@ -1,13 +1,18 @@
-import { createIsSpeakingObserver } from '@livekit/components-core';
+import { createIsSpeakingObserver, trackReference } from '@livekit/components-core';
 import type { Participant } from 'livekit-client';
+import { Track } from 'livekit-client';
 import * as React from 'react';
-import { useEnsureParticipant } from '../context';
+import { useEnsureTrackReference } from '../context';
 import { useObservableState } from './internal';
 
 export function useIsSpeaking(participant?: Participant) {
-  const p = useEnsureParticipant(participant);
-  const observable = React.useMemo(() => createIsSpeakingObserver(p), [p]);
-  const isSpeaking = useObservableState(observable, p.isSpeaking);
+  const maybeTrack = participant ? trackReference(participant, Track.Source.Unknown) : undefined;
+  const trackRef = useEnsureTrackReference(maybeTrack);
+  const observable = React.useMemo(
+    () => createIsSpeakingObserver(trackRef.participant),
+    [trackRef.participant],
+  );
+  const isSpeaking = useObservableState(observable, trackRef.participant.isSpeaking);
 
   return isSpeaking;
 }

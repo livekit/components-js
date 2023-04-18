66206fce08239b027a127f247ca0e42bbc32602a
lukasIO <mail@lukasseiler.de>
2023-04-18 14:36:09 +0200

    rever splitting

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { Options } from 'tsup';
 const defaultOptions: Options = {
   entry: ['src/index.ts'],
   format: ['cjs', 'esm'],
-  splitting: true,
+  splitting: false,
   sourcemap: true,
   treeshake: true,
   // for the type maps to work, we use tsc's declaration-only command

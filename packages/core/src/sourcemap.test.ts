9c00545ceacc5de23771a691d25c81b7acec2d9a
lukasIO <mail@lukasseiler.de>
2023-04-21 14:14:12 +0200

    wip

--- packages/core/src/sourcemap.test.ts
+++ packages/core/src/sourcemap.test.ts
@@ -1,8 +1,14 @@
 // @ts-ignore
 import validate from 'sourcemap-validator';
 import fs from 'fs';
+import { assert, describe, test } from 'vitest';
 
 const min = fs.readFileSync('./dist/index.js', 'utf-8');
 const map = fs.readFileSync('./dist/index.js.map', 'utf-8');
 
-validate(min, map, { 'index.ts': './src/index.ts' });
+describe('sourcemaps', () => {
+  test('sourcemaps are valid', () => {
+    assert.doesNotThrow(() => validate(min, map, { 'index.ts': './src/index.ts' }));
+  });
+});
+console.log();

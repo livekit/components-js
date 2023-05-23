873e6a31c59d24f04b8c7a4e6d5d344eb97e4b02
Jonas Schell <jonas@livekit.io>
2023-05-23 10:11:59 +0200

    cleanup

--- packages/react/src/hooks/useAudioPlayback.ts
+++ packages/react/src/hooks/useAudioPlayback.ts
@@ -5,9 +5,9 @@ import { roomAudioPlaybackAllowedObservable } from '@livekit/components-core';
 
 /**
  * In many browsers to start audio playback, the user must perform a user-initiated event such as clicking a button.
- * The `useAudioPlayback` hook returns an object with a boolean `canPlayAudio` flag
- * that indicates whether audio playback is allowed in the current context,
- * as well as a `startAudio` function that can be called in a button `onClick` callback to start audio playback in the current context.
+ * The `useAudioPlayback` hook returns an object with a boolean `canPlayAudio` flag that indicates whether audio
+ * playback is allowed in the current context, as well as a `startAudio` function that can be called in a button
+ * `onClick` callback to start audio playback in the current context.
  *
  * @see Autoplay policy on MDN web docs for more info: {@link https://developer.mozilla.org/en-US/docs/Web/API/Web_Audio_API/Best_practices#autoplay_policy}
  * @alpha

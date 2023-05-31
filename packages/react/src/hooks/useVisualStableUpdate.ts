aa78c1f7dc808ecb27720ee4f34416ccde894934
lukasIO <mail@lukasseiler.de>
2023-05-31 17:09:49 +0200

    move speaker detection into useVisualStableUpdate

--- packages/react/src/hooks/useVisualStableUpdate.ts
+++ packages/react/src/hooks/useVisualStableUpdate.ts
@@ -1,6 +1,7 @@
-import type { TrackReferenceOrPlaceholder } from '@livekit/components-core';
+import { TrackReferenceOrPlaceholder, getTrackReferenceId } from '@livekit/components-core';
 import { log, sortTrackReferences, updatePages } from '@livekit/components-core';
 import * as React from 'react';
+import { useSpeakingParticipants } from './useSpeakingParticipants';
 
 /** @public */
 export interface UseVisualStableUpdateOptions {
@@ -25,31 +26,36 @@ export function useVisualStableUpdate(
   maxItemsOnPage: number,
   options: UseVisualStableUpdateOptions = {},
 ): TrackReferenceOrPlaceholder[] {
-  const lastTrackRefs = React.useRef<TrackReferenceOrPlaceholder[]>([]);
   const lastMaxItemsOnPage = React.useRef<number>(-1);
   const layoutChanged = maxItemsOnPage !== lastMaxItemsOnPage.current;
 
-  const sortedTrackRefs =
-    typeof options.customSortFunction === 'function'
-      ? options.customSortFunction(trackReferences)
-      : sortTrackReferences(trackReferences);
+  const [updatedTrackRefs, setUpdatedTrackRefs] = React.useState(trackReferences);
 
-  let updatedTrackRefs: TrackReferenceOrPlaceholder[] = [...sortedTrackRefs];
-  if (layoutChanged === false) {
-    try {
-      updatedTrackRefs = updatePages(lastTrackRefs.current, sortedTrackRefs, maxItemsOnPage);
-    } catch (error) {
-      log.error('Error while running updatePages(): ', error);
+  const speakers = useSpeakingParticipants();
+
+  React.useEffect(() => {
+    const sortedTrackRefs =
+      typeof options.customSortFunction === 'function'
+        ? options.customSortFunction(trackReferences)
+        : sortTrackReferences(trackReferences);
+
+    let newTrackRefs: TrackReferenceOrPlaceholder[] = [...sortedTrackRefs];
+    if (layoutChanged === false) {
+      try {
+        newTrackRefs = updatePages(updatedTrackRefs, sortedTrackRefs, maxItemsOnPage);
+      } catch (error) {
+        log.error('Error while running updatePages(): ', error);
+      }
+      if (
+        JSON.stringify(newTrackRefs.map(getTrackReferenceId)) !==
+        JSON.stringify(updatedTrackRefs.map(getTrackReferenceId))
+      ) {
+        setUpdatedTrackRefs(newTrackRefs);
+      }
     }
-  }
 
-  // Save info for to compare against in the next update cycle.
-  if (layoutChanged) {
-    lastTrackRefs.current = sortedTrackRefs;
-  } else {
-    lastTrackRefs.current = updatedTrackRefs;
-  }
-  lastMaxItemsOnPage.current = maxItemsOnPage;
+    lastMaxItemsOnPage.current = maxItemsOnPage;
+  }, [speakers, layoutChanged, maxItemsOnPage, trackReferences, updatedTrackRefs]);
 
   return updatedTrackRefs;
 }

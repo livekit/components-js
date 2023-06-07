4e3f45e21e7be7562f6854c20d476fcdb7a48d43
Jonas Schell <jonas@livekit.io>
2023-06-07 16:01:54 +0200

    update generated icons (#518)

--- packages/react/src/assets/icons/QualityGoodIcon.tsx
+++ packages/react/src/assets/icons/QualityGoodIcon.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { SVGProps } from 'react';
+import type { SVGProps } from 'react';
 const SvgQualityGoodIcon = (props: SVGProps<SVGSVGElement>) => (
   <svg xmlns="http://www.w3.org/2000/svg" width={16} height={16} fill="currentcolor" {...props}>
     <path d="M0 11.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5zm6-5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5z" />
--- packages/react/src/assets/icons/SpinnerIcon.tsx
+++ packages/react/src/assets/icons/SpinnerIcon.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { SVGProps } from 'react';
+import type { SVGProps } from 'react';
 const SvgSpinnerIcon = (props: SVGProps<SVGSVGElement>) => (
   <svg xmlns="http://www.w3.org/2000/svg" width={16} height={16} fill="none" {...props}>
     <path

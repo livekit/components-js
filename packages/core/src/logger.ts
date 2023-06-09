50b328eaad69ed4b137e4826b4191db7a8637e78
Jonas Schell <jonas@livekit.io>
2023-06-09 14:19:42 +0200

    add setLogLevel function

--- packages/core/src/logger.ts
+++ packages/core/src/logger.ts
@@ -1,6 +1,30 @@
+import { setLogLevel as setClientSdkLogLevel } from 'livekit-client';
 import loglevel from 'loglevel';
 
 const log = loglevel.getLogger('lk-components-js');
 log.setDefaultLevel('WARN');
+
+type LogLevel = Parameters<typeof setClientSdkLogLevel>[0];
+type SetLogLevelOptions = {
+  liveKitClientLogLevel?: LogLevel;
+};
+
+/**
+ * Set the log level for both the `@livekit/components-react` package and the `@livekit-client` package.
+ * To set the `@livekit-client` log independently, use the `liveKitClientLogLevel` prop on the `options` object.
+ * @public
+ */
+export function setLogLevel(
+  level: LogLevel,
+  options: SetLogLevelOptions = { liveKitClientLogLevel: undefined },
+): void {
+  if (options.liveKitClientLogLevel) {
+    log.setLevel(level);
+    setClientSdkLogLevel(options.liveKitClientLogLevel);
+  } else {
+    log.setLevel(level);
+    setClientSdkLogLevel(level);
+  }
+}
 
 export default log;

ee21ef82b246e8c5dd42d72c506f6cd92e9a099e
lukasIO <mail@lukasseiler.de>
2023-07-05 15:35:29 +0200

    Update examples/nextjs/pages/prejoin.tsx
    Co-authored-by: Jonas Schell <jonas@livekit.io>

--- examples/nextjs/pages/prejoin.tsx
+++ examples/nextjs/pages/prejoin.tsx
@@ -3,7 +3,7 @@ import { setLogLevel } from '@livekit/components-core';
 import { LiveKitRoom, PreJoin, useToken, VideoConference } from '@livekit/components-react';
 import type { NextPage } from 'next';
 
-const MinimalExample: NextPage = () => {
+const PreJoinExample: NextPage = () => {
   //   const params = typeof window !== 'undefined' ? new URLSearchParams(location.search) : null;
   //   const roomName = params?.get('room') ?? 'test-room';
   //   const userIdentity = params?.get('user') ?? 'test-identity';

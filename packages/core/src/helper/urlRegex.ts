5d0b637672775f7354d82e2f177b45d69d2f0071
Jonas Schell <jonas@livekit.io>
2023-07-20 14:47:59 +0200

    fix some imports and linter warnings

--- packages/core/src/helper/urlRegex.ts
+++ packages/core/src/helper/urlRegex.ts
@@ -24,13 +24,13 @@ import { TLDs } from 'global-tld-list';
 interface RegExOptions {
   /**
 		Only match an exact string. Useful with `RegExp#test` to check if a string is a URL.
-		@default false
+		@defaultValue false
 		*/
   readonly exact?: boolean;
 
   /**
 		Force URLs to start with a valid protocol or `www`. If set to `false` it'll match the TLD against a list of valid [TLDs](https://github.com/stephenmathieson/node-tlds).
-		@default true
+		@defaultValue true
 		*/
   readonly strict?: boolean;
 }

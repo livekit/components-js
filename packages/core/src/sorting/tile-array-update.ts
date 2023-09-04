18f32cce86defe8b99fa4980e92943b22412c46e
Jonas Schell <jonas@livekit.io>
2023-09-04 18:00:00 +0200

    remove debug setLogLevel

--- packages/core/src/sorting/tile-array-update.ts
+++ packages/core/src/sorting/tile-array-update.ts
@@ -1,5 +1,5 @@
 import { differenceBy, chunk, zip } from '../helper/array-helper';
-import { log, setLogLevel } from '../logger';
+import { log } from '../logger';
 import type { TrackReferenceOrPlaceholder } from '../track-reference';
 import {
   getTrackReferenceId,
@@ -8,8 +8,6 @@ import {
   isTrackReferencePlaceholder,
 } from '../track-reference';
 import { flatTrackReferenceArray } from '../track-reference/test-utils';
-
-setLogLevel('debug');
 
 type VisualChanges<T> = {
   dropped: T[];

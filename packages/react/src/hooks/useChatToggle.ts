d0f12c64ed294e40703df531411deea6c6d98321
Jonas Schell <jonas@livekit.io>
2023-09-12 17:26:13 +0200

    useChatToggle

--- packages/react/src/hooks/useChatToggle.ts
+++ packages/react/src/hooks/useChatToggle.ts
@@ -8,7 +8,13 @@ export interface UseChatToggleProps {
   props: React.ButtonHTMLAttributes<HTMLButtonElement>;
 }
 
-/** @public */
+/**
+ * A React hook that provides state and functions for toggling the chat window.
+ * @remarks
+ * Depends on the `LayoutContext` to work properly.
+ * @see {@link ChatToggle}, {@link Chat}
+ * @public
+ */
 export function useChatToggle({ props }: UseChatToggleProps) {
   const { dispatch, state } = useLayoutContext().widget;
   const { className } = React.useMemo(() => setupChatToggle(), []);

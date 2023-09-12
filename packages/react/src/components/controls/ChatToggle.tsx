180e26346a9c49381e38525c9c82283ba02cfa02
Jonas Schell <jonas@livekit.io>
2023-09-12 13:44:41 +0200

    ChatToggle

--- packages/react/src/components/controls/ChatToggle.tsx
+++ packages/react/src/components/controls/ChatToggle.tsx
@@ -5,7 +5,9 @@ import { useChatToggle } from '../../hooks';
 export interface ChatToggleProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
 
 /**
- * The ChatToggle component toggles the visibility of the chat component.
+ * The `ChatToggle` component is a button that toggles the visibility of the `Chat` component.
+ * @remarks
+ * For the component to have any effect it has to live inside a `LayoutContext` context.
  *
  * @example
  * ```tsx

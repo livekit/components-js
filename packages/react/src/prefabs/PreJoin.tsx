f59bff97b3af33fd305223a106db16dccb27e26c
Jonas Schell <jonas@livekit.io>
2023-09-12 15:07:40 +0200

    PreJoin

--- packages/react/src/prefabs/PreJoin.tsx
+++ packages/react/src/prefabs/PreJoin.tsx
@@ -197,12 +197,12 @@ export function usePreviewDevice<T extends LocalVideoTrack | LocalAudioTrack>(
 }
 
 /**
- * The PreJoin prefab component is normally presented to the user before he enters a room.
+ * The `PreJoin` prefab component is normally presented to the user before he enters a room.
  * This component allows the user to check and select the preferred media device (camera und microphone).
- * On submit the user decisions are returned, which can then be passed on to the LiveKitRoom so that the user enters the room with the correct media devices.
+ * On submit the user decisions are returned, which can then be passed on to the `LiveKitRoom` so that the user enters the room with the correct media devices.
  *
  * @remarks
- * This component is independent from the LiveKitRoom component and don't has to be nested inside it.
+ * This component is independent from the `LiveKitRoom` component and don't has to be nested inside it.
  * Because it only access the local media tracks this component is self contained and works without connection to the LiveKit server.
  *
  * @example

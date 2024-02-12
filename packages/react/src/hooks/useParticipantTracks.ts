7101d5873e1224b1d6825bff89b46248c8207203
lukasIO <mail@lukasseiler.de>
2024-02-12 16:01:08 +0100

    Use identity instead of participant

--- packages/react/src/hooks/useParticipantTracks.ts
+++ packages/react/src/hooks/useParticipantTracks.ts
@@ -2,18 +2,21 @@ import * as React from 'react';
 import type { TrackReference } from '@livekit/components-core';
 import { participantTracksObservable } from '@livekit/components-core';
 import { useObservableState } from './internal';
-import type { Participant, Track } from 'livekit-client';
-import { useMaybeParticipantContext } from '../context';
+import type { Track } from 'livekit-client';
+import { useMaybeParticipantContext, useRoomContext } from '../context';
 
 /**
  * @public
  */
 export function useParticipantTracks(
   sources: Track.Source[],
-  participant?: Participant,
+  participantIdentity?: string,
 ): TrackReference[] {
+  const room = useRoomContext();
   const participantContext = useMaybeParticipantContext();
-  const p = participant ?? participantContext;
+  const p = participantIdentity
+    ? room.getParticipantByIdentity(participantIdentity)
+    : participantContext;
   const observable = React.useMemo(
     () => (p ? participantTracksObservable(p, { sources }) : undefined),
     [p?.sid, p?.identity, JSON.stringify(sources)],

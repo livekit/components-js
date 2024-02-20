38a882db89b1e045fc56026581eea530374ef065
lukasIO <mail@lukasseiler.de>
2024-02-20 09:39:09 +0100

    update api docs

--- packages/react/src/hooks/useTrack.ts
+++ packages/react/src/hooks/useTrack.ts
@@ -1,12 +1,11 @@
-import type { Participant, Track } from 'livekit-client';
-import { useTrackRefBySourceOrName } from './useTrackRefBySourceOrName';
-import { useEnsureParticipant } from '../context';
+// import type { Participant, Track } from 'livekit-client';
+// import { useTrackRefBySourceOrName } from './useTrackRefBySourceOrName';
+// import { useEnsureParticipant } from '../context';
 
 /**
  * TODO decide whether we want to expose this
  */
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-function useTrack(source: Track.Source, participant?: Participant) {
-  const p = useEnsureParticipant(participant);
-  return useTrackRefBySourceOrName({ source, participant: p });
-}
+// function useTrack(source: Track.Source, participant?: Participant) {
+//   const p = useEnsureParticipant(participant);
+//   return useTrackRefBySourceOrName({ source, participant: p });
+// }

d9d45dea56f53e662be9345087888f10600d1a4a
lukasIO <mail@lukasseiler.de>
2024-04-11 13:05:33 +0200

    cleanup

--- packages/react-native/tsup.config.ts
+++ packages/react-native/tsup.config.ts
@@ -4,6 +4,6 @@ import defaults from '../../tsup.config';
 
 export default defineConfig({
   ...defaults,
-  entry: ['src/index.ts', 'src/hooks/index.ts', 'src/prefabs/index.ts'],
-  external: ['livekit-client', 'react', 'react-dom'],
+  entry: ['src/index.ts'],
+  external: ['livekit-client', 'react', 'react-native', '@livekit/react-native'],
 });

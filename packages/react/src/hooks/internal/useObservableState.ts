5126325915c0671870fd52cb7446029162ea464f
lukasIO <mail@lukasseiler.de>
2024-05-08 16:57:09 +0200

    Reset useChat messages when room disconnects

--- packages/react/src/hooks/internal/useObservableState.ts
+++ packages/react/src/hooks/internal/useObservableState.ts
@@ -5,13 +5,20 @@ import type { Observable } from 'rxjs';
 /**
  * @internal
  */
-export function useObservableState<T>(observable: Observable<T> | undefined, startWith: T) {
+export function useObservableState<T>(
+  observable: Observable<T> | undefined,
+  startWith: T,
+  resetWhenObservableChanges = true,
+) {
   const [state, setState] = React.useState<T>(startWith);
   React.useEffect(() => {
+    if (resetWhenObservableChanges) {
+      setState(startWith);
+    }
     // observable state doesn't run in SSR
     if (typeof window === 'undefined' || !observable) return;
     const subscription = observable.subscribe(setState);
     return () => subscription.unsubscribe();
-  }, [observable]);
+  }, [observable, resetWhenObservableChanges]);
   return state;
 }

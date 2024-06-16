89ea9fe3d327156a66eea8357d7cf9b30bb37934
lukasIO <mail@lukasseiler.de>
2024-06-16 12:27:26 +0200

    docs fix

--- packages/react/src/hooks/useRemoteParticipant.ts
+++ packages/react/src/hooks/useRemoteParticipant.ts
@@ -31,6 +31,17 @@ export function useRemoteParticipant(
   identifier: ParticipantIdentifier,
   options?: UseRemoteParticipantOptions,
 ): RemoteParticipant | undefined;
+/**
+ * The `useRemoteParticipant` hook returns the first RemoteParticipant by either identity or based on the participant kind.
+ * @remarks
+ * To optimize performance, you can use the `updateOnlyOn` property to decide on what `ParticipantEvents` the hook updates.
+ *
+ * @example
+ * ```tsx
+ * const participant = useRemoteParticipant('Russ');
+ * ```
+ * @public
+ */
 export function useRemoteParticipant(
   identity: string,
   options?: UseRemoteParticipantOptions,

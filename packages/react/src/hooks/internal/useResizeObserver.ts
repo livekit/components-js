93a6bd3172c83790c82fb859ed9038e04bb106f7
lukasIO <mail@lukasseiler.de>
2024-07-10 15:09:26 +0200

    dont export helper

--- packages/react/src/hooks/internal/useResizeObserver.ts
+++ packages/react/src/hooks/internal/useResizeObserver.ts
@@ -10,7 +10,6 @@ const useLatest = <T>(current: T) => {
   return storedValue;
 };
 
-export default useLatest;
 /**
  * A React hook that fires a callback whenever ResizeObserver detects a change to its size
  * code extracted from https://github.com/jaredLunde/react-hook/blob/master/packages/resize-observer/src/index.tsx in order to not include the polyfill for resize-observer

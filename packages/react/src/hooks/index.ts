5bad07d09198cb08bde328d98d24652e250bf6b2
lukasIO <mail@lukasseiler.de>
2024-10-01 03:26:11 +0200

    Update imports

--- packages/react/src/hooks/index.ts
+++ packages/react/src/hooks/index.ts
@@ -54,4 +54,3 @@ export * from './useTrackTranscription';
 export * from './useVoiceAssistant';
 export * from './useParticipantAttributes';
 export * from './useIsRecording';
-export { useKrispNoiseFilter, useKrispNoiseFilterOptions } from './cloud/krisp/useKrispNoiseFilter';

ea2ad896520c33d2271814a59d9c08d856bb8e65
lukasIO <mail@lukasseiler.de>
2024-10-01 13:40:13 +0200

    avoid using react plugin

--- packages/react/vite.config.js
+++ packages/react/vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  appType: 'custom',
   plugins: [
-    react(),
+    // react(),
     dts({
       insertTypesEntry: true, // Create a `types` entry in package.json
       rollupTypes: false, // Bundle .d.ts files into a single declaration file

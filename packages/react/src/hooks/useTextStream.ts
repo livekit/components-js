cd44bbb13adbaf869c890cc942dde0af8d88a277
lukasIO <mail@lukasseiler.de>
2025-03-26 14:58:12 +0100

    add usage example

--- packages/react/src/hooks/useTextStream.ts
+++ packages/react/src/hooks/useTextStream.ts
@@ -5,6 +5,16 @@ import { useRoomContext } from '../context';
 import { useConnectionState } from './useConnectionStatus';
 import { useObservableState } from './internal';
 
+/**
+ * @beta
+ * @param topic - the topic to listen to
+ * @returns an array of TextStreamData that holds the text, participantInfo, and streamInfo
+ * @example
+ * ```tsx
+ * const { textStreams } = useTextStream('my-topic');
+ * return <div>{textStreams.map((textStream) => textStream.text)}</div>;
+ * ```
+ */
 export function useTextStream(topic: string) {
   const room = useRoomContext();
 

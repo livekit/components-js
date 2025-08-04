34e09cd00c049779920a4227b8d435edef56e6ea
Ryan Gaus <rsg1egoman@gmail.com>
2025-08-04 14:50:52 -0400

    feat: add new useRoomConnection hook for managing room connections
    It seems initially like modelling a room connection with a `useEffect(()
    => { room.connect(); return () => room.disconnect(); }, [...])` would be
    natural and work well, however if dependencies in that `useEffect` change
    rapidly, then the room can thrash back and forth between connected and
    disconnected, and because room.disconnect() is async, sometimes
    resulting in connects and disconnects happening at the same time.
    
    There's some thinking being done to address this at the client-sdk-js
    level but for now, introduce a hook folks can use to at least stem the
    bleeding a bit. Once the issue is fixed in `client-sdk-js` then this
    hook can be updated to just use a `useEffect` again.

---
+++ packages/react/src/hooks/useRoomConnection.ts
@@ -0,0 +1,89 @@
+import { Mutex, TrackPublishOptions, Room } from 'livekit-client';
+import { useEffect, useMemo, useState } from 'react';
+import { useMaybeRoomContext } from '../context';
+
+type UseRoomConnectionDetails = {
+  serverUrl: string;
+  participantToken: string;
+};
+
+type UseRoomConnectionOptions = {
+  getConnectionDetails: () => Promise<UseRoomConnectionDetails>;
+  onConnectionError?: (err: Error) => void;
+
+  room?: Room;
+  /** Should the room attempt to connect? If false, the room will disconnect. */
+  connected?: boolean;
+  trackPublishOptions?: TrackPublishOptions;
+};
+
+type UseRoomConnectionResult = {
+  room: Room,
+
+  /** What operation is the useRoomConnection hook currently in the midst of performing?  */
+  status: 'idle' | 'connecting' | 'disconnecting',
+};
+
+/**
+ * The `useRoomConnection` hook provides a fully managed way to 
+ * @remarks
+ * Can be called inside a `RoomContext` or by passing a `Room` instance, otherwise creates a Room
+ * itself.
+ *
+ * @example
+ * ```tsx
+ * const { room } = useRoomConnection({
+ *   getConnectionDetails: async () => {
+ *     // compute the below value out of band:
+ *     return { serverUrl: "...", participantToken: "..." };
+ *   },
+ * });
+ * ```
+ * @public
+ */
+export function useRoomConnection(options: UseRoomConnectionOptions): UseRoomConnectionResult {
+  const roomFromContext = useMaybeRoomContext();
+  const room = useMemo(() => {
+    return roomFromContext ?? options.room ?? (new Room());
+  }, [options.room, roomFromContext]);
+
+  const connected = options.connected ?? true;
+
+  const [status, setStatus] = useState<'idle' | 'connecting' | 'disconnecting'>('idle');
+
+  // NOTE: it would on the surface seem that managing a room's connection with a useEffect would be
+  // straightforward, but `room.disconnect()` is async and useEffect doesn't support async cleanup
+  // functions, which means `room.connect()` can run in the midst of `room.disconnect()`, causing
+  // race conditions.
+  const connectDisconnectLock = useMemo(() => new Mutex(), []);
+  useEffect(() => {
+    connectDisconnectLock.lock().then(async (unlock) => {
+      if (connected) {
+        setStatus('connecting');
+        const connectionDetails = await options.getConnectionDetails();
+        try {
+          await Promise.all([
+            room.localParticipant.setMicrophoneEnabled(true, undefined, options.trackPublishOptions),
+            room.connect(connectionDetails.serverUrl, connectionDetails.participantToken),
+          ]);
+        } catch (error) {
+          options.onConnectionError?.(error as Error);
+        } finally {
+          setStatus('idle');
+        }
+      } else {
+        setStatus('disconnecting');
+        try {
+          await room.disconnect();
+        } catch (error) {
+          options.onConnectionError?.(error as Error);
+        } finally {
+          setStatus('idle');
+        }
+      };
+      unlock();
+    });
+  }, [connected]);
+
+  return { room, status };
+}

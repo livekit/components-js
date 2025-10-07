683203178a90c1eb09f61d62787578a8adc44e30
Ryan Gaus <rsg1egoman@gmail.com>
2025-10-07 12:04:54 -0400

    feat: add new SessionProvider component

---
+++ packages/react/src/components/SessionProvider.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import { UseSessionReturn } from '../hooks';
+import { RoomContext, SessionContext } from '../context';
+// NOTE: the below `useSession` is mentioned in a tsdoc comment
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { useSession } from '../hooks';
+
+/** @public */
+export type SessionProviderProps = {
+  session: UseSessionReturn;
+  children: React.ReactNode;
+};
+
+/**
+ * The `SessionProvider` component instantiates a SessionContext from the return of {@link useSession}
+ * @public
+ */
+export function SessionProvider(props: SessionProviderProps) {
+  return (
+    <SessionContext.Provider value={props.session}>
+      <RoomContext.Provider value={props.session.room}>{props.children}</RoomContext.Provider>
+    </SessionContext.Provider>
+  );
+}

4b61a001054dd867b3fe0a2bfc225645383f6345
Thomas Yuill <thomas.yuill@livekit.io>
2026-02-27 10:04:05 -0500

    wrap licenses in jsdoc tag

--- packages/shadcn/components/agents-ui/agent-audio-visualizer-aura.tsx
+++ packages/shadcn/components/agents-ui/agent-audio-visualizer-aura.tsx
@@ -1,10 +1,14 @@
-// Originally developed for Unicorn Studio
-// https://unicorn.studio
-//
-// Licensed under the Polyform Non-Resale License 1.0.0
-// https://polyformproject.org/licenses/non-resale/1.0.0/
-//
-// © 2026 UNCRN LLC
+/**
+ * @license
+ *
+ * Originally developed for Unicorn Studio
+ * https://unicorn.studio
+ *
+ * Licensed under the Polyform Non-Resale License 1.0.0
+ * https://polyformproject.org/licenses/non-resale/1.0.0/
+ *
+ * © 2026 UNCRN LLC
+ */
 
 'use client';
 
